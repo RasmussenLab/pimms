18f89904c8d5e2907d7f0f7f4870dfab8f93de82
enryh <henry.webel@sund.ku.dk>
2020-01-22 10:03:00 +0100

    scripts for development

--- project/bin/Mann_Hepa_data.py
+++ project/bin/Mann_Hepa_data.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import logging
+from vaep.utils import load_data
 
 logger = logging.getLogger('test')
 logger.setLevel(logging.DEBUG)
@@ -14,9 +15,11 @@ FOLDER = 'data'
 FILE = 'Mann_Hepa_data.tsv'
 
 _file = os.path.join(FOLDER, FILE)
-df = pd.read_csv(_file, sep='\t', index_col='index')
-meta_data = df.iloc[:,:5]
-proteins = df.iloc[:,5:]
+# df = pd.read_csv(_file, sep='\t', index_col='index')
+# meta_data = df.iloc[:,:5]
+# proteins = df.iloc[:,5:]
+
+meta_data, proteins = load_data(_file)
 
 info_na = proteins.isna().sum().describe(percentiles=[x/100 for x in range(0,100,5)] )
 
