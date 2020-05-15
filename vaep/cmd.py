e6bebbe15d7397eb07cc945f6439986f8cfa5150
enryh <henry.webel@sund.ku.dk>
2020-05-15 12:29:59 +0200

    re-order notebooks, add parser

---
+++ vaep/cmd.py
@@ -0,0 +1,19 @@
+# io module?
+import argparse
+import torch
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description='VAE MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    args = parser.parse_args(args)
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    return args
