4b53c28224e9abc0e081d9f4ec363ef16504d206
enryh <henry.webel@cpr.ku.dk>
2020-09-23 21:21:08 +0200

    count peptide matches to level (e.g. gene)

--- vaep/fasta.py
+++ vaep/fasta.py
@@ -4,6 +4,11 @@ and created by Johannes B. Müller
 [scholar](https://scholar.google.com/citations?user=Rn1OS8oAAAAJ&hl=de),
 [MPI Biochemistry](https://www.biochem.mpg.de/person/93696/2253)
 """
+import logging
+
+from tqdm import tqdm
+
+logger = logging.getLogger('vaep.fasta')
 
 
 def get_n_miscleaved(pep_sequences: list, num_missed: int):
@@ -92,3 +97,67 @@ def iterFlatten(root):
                 yield e
     else:
         yield root
+
+
+def count_peptide_matches(peptide_to_proteinID: dict,
+                          protein_to_gene: dict = None,
+                          level: str = 'protein_id') -> dict:
+    """Count the number of matches of a peptide to the specified level.
+    Provides the basis for summary statistics (a counter of matches).
+
+    Possibly to be extended in a class handling the matches of proteinIDs to
+    peptide sequences.
+
+    Parameters
+    ----------
+    peptide_to_proteinID : dict
+        mapping of peptides to a list of proteinIDs.
+    protein_to_gene : dict, optional
+        Uniport mapping of protein IDs (no isotopes ending "-2", "-3", etc)
+        in case of level='gene', by default None
+    level : str, optional
+        to which level the peptides should be matched.
+        'protein_id', 'protein' or 'gene', by default 'protein_id'
+
+    Returns
+    -------
+    dict
+        Counter of number of matches of peptides to the specified level items.
+        {1 : 3, 3: 5} means that 3 peptides have been match uniquly to one item (e.g. gene)
+        and 5 peptides have been matched to 3 items (e.g. genes).
+
+    Raises
+    ------
+    KeyError
+        [description]
+    """
+    assert level in ['protein_id', 'protein', 'gene'], ValueError(
+        'Specify one of the three (in order of aggregation level): {}'.format(
+            ', '.join(['prot_id', 'prot', 'gene'])))
+    if level == 'gene':
+        assert protein_to_gene is not None, "Please provide protein to gene level name"
+        _set_missing_entires = set()
+    n_peptides_mapped_to_level = {}
+
+    for _pep, _protein_ids in tqdm(peptide_to_proteinID.items()):
+
+        if level == 'protein_id':
+            n_peptides_mapped_to_level[_pep] = len(_protein_ids)
+        elif level == 'protein':
+            n_peptides_mapped_to_level[_pep] = len(
+                {x.split('-')[0] for x in peptide_to_proteinID[_pep]})
+        elif level == 'gene':
+            _proteins = {x.split('-')[0] for x in peptide_to_proteinID[_pep]}
+            _set_genes = set()
+            for _prot in _proteins:
+                try:
+                    _set_genes.add(protein_to_gene[_prot])
+                except KeyError:
+                    _set_missing_entires.add(_prot)
+            n_peptides_mapped_to_level[_pep] = len(_set_genes)
+        else:
+            raise KeyError(f'unknown level: {level}')
+    if level == 'gene':
+        logger.warning(
+            f'Missing protein to gene mapppings: {len(set(_set_missing_entires))}')
+    return n_peptides_mapped_to_level
