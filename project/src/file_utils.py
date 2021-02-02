689fddd91e86f0cf6422620a5b06a4561ab803ac
Henry Webel <henry.webel@cpr.ku.dk>
2021-02-02 23:24:09 +0100

    move more functionality to vaep, remove from file_utils

--- project/src/file_utils.py
+++ project/src/file_utils.py
@@ -45,64 +45,6 @@ def check_for_key(iterable, key):
         return '_'.join(iterable)
 
 
-PathsList = namedtuple('PathsList', ['files', 'folder'])
-
-
-def search_files(path='.', query='.txt'):
-    """Uses Pathlib to find relative to path files 
-    with the query text in their file names. Returns 
-    the path relative to the specified path.
-
-    Parameters
-    ----------
-    path : str, optional
-        Path to search, by default '.'
-    query : str, optional
-        query string for for filenames, by default '.txt'
-
-    Returns
-    -------
-    list
-        list with files as string containig query key. 
-    """
-    path = Path(path)
-    files = []
-    for p in path.rglob("*"):
-        if query in p.name:
-            files.append(str(p.relative_to(path)))
-    return PathsList(files=files, folder=path)
-
-
-def search_subfolders(path='.', depth: int = 1, exclude_root: bool = False):
-    """Search subfolders relative to given path."""
-    if not isinstance(depth, int) and depth > 0:
-        raise ValueError(
-            f"Please provide an strictly positive integer, not {depth}")
-    EXCLUDED = ["*ipynb_checkpoints*"]
-    
-    path = Path(path)
-    directories = [path]
-
-    def get_subfolders(path):
-        return [x for x in path.iterdir() 
-                if x.is_dir() and 
-                not any(x.match(excl) for excl in EXCLUDED)
-               ]
-
-    directories_previous = directories.copy()
-    while depth > 0:
-        directories_new = list()
-        for p in directories_previous:
-            directories_new.extend(
-                   get_subfolders(p))
-        directories.extend(directories_new)
-        directories_previous = directories_new.copy()
-        depth -= 1
-    
-    if exclude_root:
-        directories.pop(0)
-    return directories
-
 # can file-loading be made concurrent?
 # check tf.data
 
@@ -242,22 +184,22 @@ types_peptides = {'N-term cleavage window': dtype('O'),
                   'MS/MS Count': dtype('int64')}
 
 
-def load_peptide_intensities(filepath):
-    f"""Load Intensities from `peptides.txt`.
-    Data types of columns as of in MaxQuant {MQ_VERSION}
+# def load_peptide_intensities(filepath):
+#     f"""Load Intensities from `peptides.txt`.
+#     Data types of columns as of in MaxQuant {MQ_VERSION}
 
-    Parameters
-    ----------
-    filepath : str
-        filepath (rel or absolute) to MQ peptides.txt
+#     Parameters
+#     ----------
+#     filepath : str
+#         filepath (rel or absolute) to MQ peptides.txt
 
-    Returns
-    -------
-    pandas.DataFrame
-        Return text file as DataFrame.
-    """
-    df = pd.read_table(filepath, index_col='Sequence', dtype=types_peptides)
-    return df[['Intensity']]
+#     Returns
+#     -------
+#     pandas.DataFrame
+#         Return text file as DataFrame.
+#     """
+#     df = pd.read_table(filepath, index_col='Sequence', dtype=types_peptides)
+#     return df[['Intensity']]
 
 
 dtypes_proteins = {'Protein IDs': dtype('O'),
@@ -317,162 +259,3 @@ def load_protein_intensities(filepath):
 
 
 
-
-class MaxQuantOutput:
-    """Class assisting with MaxQuant txt output folder.
-
-    Parameters
-    ----------
-    folder: pathlib.Path, str
-        Path to Maxquant `txt` output folder.
-
-   
-    Attributes
-    ----------
-    self.files : list 
-        list of files in `folder`.
-    _inital_attritubutes : list
-        Initial set of non-magic attributes 
-    NAME_FILE_MAP : dict
-        Keys for known MaxQuant output files.
-    """
-    NAME_FILE_MAP = {'allPeptides': 'allPeptides.txt',
-             'evidence': 'evidence.txt',
-             'matchedFeatures': 'matchedFeatures.txt',
-             'modificationSpecificPeptides': 'modificationSpecificPeptides.txt',
-             'ms3Scans': 'ms3Scans.txt',
-             'msms': 'msms.txt',
-             'msmsScans': 'msmsScans.txt',
-             'mzRange': 'mzRange.txt',
-             'OxidationSites': 'Oxidation (M)Sites.txt',
-             'parameters': 'parameters.txt',
-             'peptides': 'peptides.txt',
-             'proteinGroups': 'proteinGroups.txt',
-             'summary': 'summary.txt'}
-   
-    def __init__(self, folder):
-        self.folder = Path(folder)
-        self.files = self.get_files()
-        
-    
-    def get_files(self):
-        """Get all txt files in output folder
-        
-        Attributes
-        ---------
-        paths: NamedTuple
-        """
-        self.paths = search_files(path=self.folder, query='.txt')
-        return self.paths.files
-    
-    @classmethod
-    def register_file(cls, filename):
-        
-        @property
-        def fct(cls):
-            return cls.find_attribute(f'_{filename}')
-        
-        return fct
-    
-    def find_attribute(self, filename):
-        """Look up or load attribute."""
-        if not hasattr(self, filename):
-            df = self.load(filename[1:])
-            setattr(self, filename, df)
-        return getattr(self, filename)
-        
-    def load(self, file):
-        """Load a specified file into memory and return it.
-        Can be used """
-        filepath = self.folder / self.NAME_FILE_MAP[file]
-        if not Path(filepath).exists():
-            raise FileNotFoundError(f"No such file: {file}.txt: Choose one of the following {', '.join(self.files)}")
-        
-        return pd.read_table(filepath, index_col=0) 
-    
-    # needed to reset attributes on instance creation.
-    _inital_attritubutes = [x for x in dir() if not x.startswith('__')]
-
-    def get_list_of_attributes(self):
-        """Return current list on non-magic instance attributes."""
-        return [x for x in dir(self) if not x.startswith('__')]
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}({self.folder!r})'
-
-# register all properties
-# Would be great to be able to do this at runtime based on the files actually present.
-for filename in MaxQuantOutput.NAME_FILE_MAP.keys():
-    setattr(MaxQuantOutput, filename, MaxQuantOutput.register_file(filename))
-
-# This version offers less inspection possibilities as the attributes are only set when they are looked up.
-class MaxQuantOutputDynamic:
-    """Class assisting with MaxQuant txt output folder. Fetches only availabe txt files.
-    
-    Parameters
-    ----------
-    folder: pathlib.Path, str
-        Path to Maxquant `txt` output folder.
-
-    Attributes
-    ---------
-    files : list
-        file names on disk
-    file_keys : list
-        keys for file name on disk to use for lookup
-    name_file_map : dict
-        Keys for known MaxQuant output files.
-    _inital_attritubutes : list
-        Initial set of non-magic attributes 
-    """  
-    def __init__(self, folder):
-        self.folder = Path(folder)
-        self.files = self.get_files()
-        
-        # patch properties at instance creation?
-        self.name_file_map = {}
-        for file in self.files:
-             file_key = Path(file).stem
-             for symbol in " ()":
-                 file_key = file_key.replace(symbol, '')
-             self.name_file_map[file_key] = file
-        self.file_keys = list(self.name_file_map)
-    
-    def get_files(self):
-        """Get all txt files in output folder
-        
-        Attributes
-        ---------
-        paths: NamedTuple
-        """
-        self.paths = search_files(path=self.folder, query='.txt')
-        return self.paths.files
-    
-        
-    def load(self, filename):
-        """Load a specified file into memory and return it.
-        Can be used """
-        filepath = self.folder / self.name_file_map[filename]
-        if not Path(filepath).exists():
-            raise FileNotFoundError(f"No such file: {filename}.txt: Choose one of the following:\n{', '.join(self.files)}")
-        
-        return pd.read_table(filepath, index_col=0) 
-    
-    # needed to reset attributes on instance creation.
-    _inital_attritubutes = [x for x in dir() if not x.startswith('__')]
-
-    def get_list_of_attributes(self):
-        """Return current list on non-magic instance attributes."""
-        return [x for x in dir(self) if not x.startswith('__')]
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}({self.folder!r})'
-    
-    def __getattr__(self, filename):
-        if filename in self.name_file_map:
-            df = self.load(filename)
-            setattr(self, filename, df)
-        else:
-            msg = f"No such file: {filename}.txt: Choose one of the following:\n{', '.join(self.file_keys)}"
-            raise AttributeError(msg)
-        return df
