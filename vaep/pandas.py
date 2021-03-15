a59feba75102d382dc187e505b5011f5553fa79a
Henry Webel <henry.webel@cpr.ku.dk>
2021-03-15 11:41:32 +0100

    :bulb: docstrings for vaep.pandas

--- vaep/pandas.py
+++ vaep/pandas.py
@@ -1,3 +1,4 @@
+from types import SimpleNamespace
 import pandas as pd
 
 
@@ -6,9 +7,63 @@ def combine_value_counts(X: pd.DataFrame, dropna=True):
     
     This performs no checks. Make sure the scale of the variables
     you pass is comparable.
+
+    Parameters
+    ----------
+    X : pandas.DataFrame
+        A DataFrame of several columns with values in a similar range.
+    dropna : bool, optional
+        Exclude NA values from counting, by default True
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame of combined value counts.
+    """    
+    """
     """
     _df = pd.DataFrame()
     for col in X.columns:
         _df = _df.join(X[col].value_counts(dropna=dropna), how='outer')
     freq_targets = _df.sort_index()
     return freq_targets
+
+
+def unique_cols(s: pd.Series):
+    """Check all entries are equal in pandas.Series
+    
+    Ref: https://stackoverflow.com/a/54405767/968487
+
+    Parameters
+    ----------
+    s : pandas.Series
+        Series to check uniqueness
+
+    Returns
+    -------
+    bool
+        Boolean on if all values are equal.
+    """    
+    return (s.iloc[0] == s).all()
+
+
+def get_unique_non_unique_columns(df: pd.DataFrame):
+    """Get back a namespace with an column.Index both
+    of the unique and non-unique columns.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+    
+    Returns
+    -------
+    types.SimpleNamespace
+        SimpleNamespace with `unique` and `non_unique` column names indices.
+    """    
+
+    mask_unique_columns = df.apply(unique_cols)
+    
+    columns = SimpleNamespace()
+    columns.unique = df.columns[mask_unique_columns]
+    columns.non_unique = df.columns[~mask_unique_columns]
+    return columns
