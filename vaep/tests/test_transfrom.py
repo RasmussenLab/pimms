8abd43c5592a35f62c9dd09b56e7c79956e01bc6
Henry <henry.webel@cpr.ku.dk>
2021-05-11 13:40:45 +0200

    sklearn StandardScaler for pandas.DataFrame

--- vaep/tests/test_transfrom.py
+++ vaep/tests/test_transfrom.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import numpy as np 
+import numpy.testing as npt
+from sklearn import preprocessing
+
 from vaep.transform import log
+from vaep.transform import StandardScaler
 
 def test_log():
     row = pd.Series([np.NaN, 0.0, np.exp(1), np.exp(2)])
     row = log(row)
     assert row.equals(pd.Series([np.NaN, np.NaN, 1.0, 2.0]))
+
+
+def test_StandardScaler():    
+    X = pd.DataFrame(np.array([[2,None], [3,2], [4,6]]))
+    npt.assert_almost_equal(
+        preprocessing.StandardScaler().fit(X).transform(X),
+        StandardScaler().fit(X).transform(X).to_numpy()
+    )
--- vaep/transform.py
+++ vaep/transform.py
@@ -1,6 +1,38 @@
 import pandas as pd
 import numpy as np
+from sklearn import preprocessing
 
 def log(row: pd.Series):
     """Apply log Transformation to values."""
     return np.log(row.where(row != 0.0))
+
+
+
+# ## Avoid tedious post-processing:
+# ## - numpy array variant
+# analysis.corr_linear_vs_log = pd.DataFrame(
+#     scaler.transform(X=analysis.df),
+#     columns = analysis.df.columns
+# ).corrwith(
+#     other=pd.DataFrame(
+#         scaler_log.transform(X_log10),
+#         columns = analysis.df.columns
+#     ),
+#     axis=0)
+# analysis.corr_linear_vs_log.describe()
+class StandardScaler(preprocessing.StandardScaler):
+    def transform(self, X, copy=None):
+        res = super().transform(X, copy)
+        if isinstance(X, pd.DataFrame):
+            return pd.DataFrame(res, columns=X.columns, index=X.index)
+        return res
+
+    def inverse_transform(self, X, copy=None):
+        res = super().inverse_transform(X, copy)
+        if isinstance(X, pd.DataFrame):
+            return pd.DataFrame(res, columns=X.columns, index=X.index)
+        return res
+
+
+def get_df_fitted_mean_std(self, index):
+    return pd.DataFrame({'mean': self.mean_, 'stddev': self.scale_}, index=index)
