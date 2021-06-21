583c0cc9f0cca61c7e2efb64a1589089fa66396a
Henry <henry.webel@cpr.ku.dk>
2021-06-21 14:23:34 +0200

    :recycle: mv code to cls

--- project/src/analyzers.py
+++ project/src/analyzers.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 class Analysis(SimpleNamespace):
-    
+
     def __repr__(self):
         ret = super().__repr__()
         return ret
@@ -23,11 +23,12 @@ class AnalyzePeptides(SimpleNamespace):
         Some statistics of certain aspects. Normally each will be a DataFrame.
 
     Many more attributes are set dynamically depending on the concrete analysis.
-    """    
+    """
 
     def __init__(self, fname, nrows=None):
         self.df = self.read_csv(fname, nrows=nrows)
         self.stats = SimpleNamespace()
+        self.is_log_transformed = False
 
     def read_csv(self, fname, nrows):
         return pd.read_csv(fname, index_col=0, low_memory=False, nrows=nrows)
@@ -42,10 +43,47 @@ class AnalyzePeptides(SimpleNamespace):
         self.stats.peptides = stats
         return stats
 
+    def log_transform(self, log_fct:np.ufunc):
+        """Log transform data in-place.
+
+        Parameters
+        ----------
+        log_fct : np.ufunc
+            Numpy log-function
+
+        Raises
+        ------
+        Exception
+            if data has been previously log-transformed.
+        """
+        if self.is_log_transformed:
+            raise Exception(
+                f'Data was already log transformed, using {self.__class__.__name__}.log_fct: {self.log_fct}')
+        else:
+            self.df = log_fct(self.df)
+            self.is_log_transformed = True
+            self.log_fct = log_fct
+            
+
+    def get_dectection_limit(self):
+        """Compute information on detection limit in dataset.
+
+        Returns
+        -------
+        str
+            Information on detection limit
+        """
+        self.detection_limit = self.df.min().min() if self.is_log_transformed else np.log10(
+            self.df).min().min()  # all zeros become nan.
+        return "Detection limit: {:6.3f}, corresponding to intensity value of {:,d}".format(
+            self.detection_limit,
+            int(10 ** self.detection_limit))
+
     def __repr__(self):
         keys = sorted(self.__dict__)
         items = ("{}".format(k, self.__dict__[k]) for k in keys)
         return "{} with attributes: {}".format(type(self).__name__, ", ".join(items))
+
 
 def corr_lower_triangle(df):
     """Compute the correlation matrix, returning only unique values."""
