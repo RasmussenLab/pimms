6cabadbd6efa4ebb469e21b91a806182d755af38
Henry <henry.webel@cpr.ku.dk>
2021-09-08 14:17:11 +0200

    :art: refactor PCA plot to method of AnalyzePeptides
    - df_meta created as attribute, using method
    - create whole plot as one method call

--- project/src/analyzers.py
+++ project/src/analyzers.py
@@ -1,14 +1,23 @@
+from collections import namedtuple
 from types import SimpleNamespace
+import itertools
 
 import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import seaborn
 
 from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectFwe
+from sklearn.impute import SimpleImputer
+
 
 from vaep.pandas import _add_indices
+
+from . import metadata
+
 
 class Analysis(SimpleNamespace):
 
@@ -34,7 +43,7 @@ class AnalyzePeptides(SimpleNamespace):
         self.df = self.read_csv(fname, nrows=nrows)
         self.N, self.M = self.df.shape
         assert f'N{self.N:05d}' in str(fname) and f'M{self.M:05d}' in str(fname), \
-         f"Filename number don't match loaded numbers: {fname} should contain N{self.N} and M{self.M}"
+            f"Filename number don't match loaded numbers: {fname} should contain N{self.N} and M{self.M}"
         self.stats = SimpleNamespace()
         self.is_log_transformed = False
 
@@ -51,7 +60,71 @@ class AnalyzePeptides(SimpleNamespace):
         self.stats.peptides = stats
         return stats
 
-    def log_transform(self, log_fct:np.ufunc):
+    def get_prop_not_na(self):
+        """Get prop. of not NA values for each sample."""
+        return self.df.notna().sum(axis=1) / self.df.shape[-1]
+
+    def add_metadata(self):
+        d_meta = metadata.get_metadata_from_filenames(self.df.index)
+        self.df_meta = pd.DataFrame.from_dict(
+            d_meta, orient='index')
+        print(f'Created metadata DataFrame attribute `df_meta`.')
+        # add proportion on not NA to meta data
+        self.df_meta['prop_not_na'] = self.get_prop_not_na()
+        print(f'Added proportion of not NA values based on `df` intensities.')
+        return self.df_meta
+
+    def plot_pca(self,):
+        """Create principal component plot with three heatmaps showing
+        instrument, degree of non NA data and sample by date."""
+        if not hasattr(self, 'df_meta'):
+            _ = self.add_metadata()
+
+        X = SimpleImputer().fit_transform(self.df)
+        X = _add_indices(X, self.df)
+        assert X.isna().sum().sum() == 0
+
+        pca = run_pca(X)
+        cols = list(pca.columns)
+
+        fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(
+            15, 20), constrained_layout=True)
+
+        Dim = namedtuple('DimensionsData', 'N M')
+        self.dim = Dim(*self.df.shape)
+
+        fig.suptitle(
+            f'First two Principal Components of {self.dim.M} most abundant peptides \n for {self.dim.N} samples', fontsize=30)
+
+        # by instrument
+        ax = axes[0]
+        pca['ms_instrument'] = self.df_meta['ms_instrument'].astype('category')
+        # for name, group in pca.groupby('ms_instrument'):
+        #     ax.scatter(x=group[cols[0]], y=group[cols[1]], label=name)
+        seaborn.scatterplot(x=pca[cols[0]], y=pca[cols[1]],
+                            hue=pca['ms_instrument'], ax=ax, palette='deep')
+        ax.set_title('by category', fontsize=18)
+        ax.legend(loc='center right', bbox_to_anchor=(1.11, 0.5))
+
+        # by complettness/missingness
+        # continues colormap will be a bit trickier using seaborn: https://stackoverflow.com/a/44642014/9684872
+        ax = axes[1]
+        ax.set_title('by number on na', fontsize=18)
+        ax.set_xlabel(cols[0])
+        ax.set_ylabel(cols[1])
+        path_collection = ax.scatter(
+            x=cols[0], y=cols[1], c=self.df_meta['prop_not_na'], data=pca)
+        _ = fig.colorbar(path_collection, ax=ax)
+
+        # by dates
+        ax = axes[2]
+        ax.set_title('by date', fontsize=18)
+        path_collection = scatter_plot_w_dates(
+            ax, pca, dates=self.df_meta.date, errors='raise')
+        path_collection = add_date_colorbar(path_collection, ax=ax, fig=fig)
+        return fig
+
+    def log_transform(self, log_fct: np.ufunc):
         """Log transform data in-place.
 
         Parameters
@@ -71,7 +144,6 @@ class AnalyzePeptides(SimpleNamespace):
             self.df = log_fct(self.df)
             self.is_log_transformed = True
             self.log_fct = log_fct
-            
 
     def get_dectection_limit(self):
         """Compute information on detection limit in dataset.
@@ -96,6 +168,7 @@ class AnalyzePeptides(SimpleNamespace):
     def fname_stub(self):
         assert hasattr(self, 'df'), f'Attribute df is missing: {self}'
         return 'N{:05d}_M{:05d}'.format(*self.df.shape)
+
 
 def corr_lower_triangle(df):
     """Compute the correlation matrix, returning only unique values."""
@@ -160,8 +233,9 @@ def scatter_plot_w_dates(ax, df, dates=None, errors='raise'):
     )
     return path_collection
 
+
 def add_date_colorbar(mappable, ax, fig):
     loc = mdates.AutoDateLocator()
     _ = fig.colorbar(mappable, ax=ax, ticks=loc,
-                 format=mdates.AutoDateFormatter(loc))
+                     format=mdates.AutoDateFormatter(loc))
     return ax
