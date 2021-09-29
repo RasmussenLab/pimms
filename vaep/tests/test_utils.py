8eba9c478f207f8dc619e101e16f64d859cd9605
enryh <henry.webel@cpr.ku.dk>
2021-09-29 11:39:49 +0200

    :sparkles: create function of appending suffix to filename
    - export common procedure into utils

---
+++ vaep/tests/test_utils.py
@@ -0,0 +1,16 @@
+import pathlib
+from vaep.utils import append_to_filepath, sample_iterable
+
+# def test_sample_iterable():
+#     pass
+
+
+def test_append_to_filepath():
+    fp = pathlib.Path('data/experiment_data.csv')
+
+    fp_new = pathlib.Path('data/experiment_data_processed.csv')
+    assert append_to_filepath(filepath=fp, to_append='processed') == fp_new
+
+    fp_new = pathlib.Path('data/experiment_data_processed.pkl')
+    assert append_to_filepath(
+        filepath=fp, to_append='processed', new_suffix='pkl') == fp_new
--- vaep/utils.py
+++ vaep/utils.py
@@ -1,4 +1,5 @@
 from random import sample
+import pathlib
 
 
 def sample_iterable(iterable: dict, n=10):
@@ -6,3 +7,18 @@ def sample_iterable(iterable: dict, n=10):
     n_examples_ = n if len(iterable) > n else len(iterable)
     sample_ = sample(iterable, n_examples_)
     return sample_
+
+
+def append_to_filepath(filepath: pathlib.Path,
+                       to_append: str,
+                       sep: str = '_',
+                       new_suffix: str = None):
+    """Append filepath with specified to_append using a seperator. 
+    
+    Example: `data.csv` to data_processed.csv
+    """
+    suffix = filepath.suffix
+    if new_suffix:
+        suffix = f".{new_suffix}"
+    new_fp = filepath.parent / f'{filepath.stem}{sep}{to_append}{suffix}'
+    return new_fp
