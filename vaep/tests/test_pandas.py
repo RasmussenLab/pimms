2a3ed0bc62d2b2ee185305b26c702cdb4b7794c5
Henry <henry.webel@cpr.ku.dk>
2021-11-08 16:06:33 +0100

    :refactor:bug: extend interpolation function
    - fix bug: first and last row were overwritten in original data (added copy)
    - fix bug: Two leading NAs were not imputed (special case)
    - move interpolation (on replica) to file and add test
    - save long_format to file for further usage

---
+++ vaep/tests/test_pandas.py
@@ -0,0 +1,32 @@
+from numpy import nan
+import pandas as pd
+from vaep.pandas import interpolate
+
+test_data = {
+    "pep1": {0: nan, 1: 27.8, 2: 28.9, 3: nan, 4: 28.7},
+    "pep2": {0: 29.1, 1: nan, 2: 27.6, 3: 29.1, 4: nan},
+    "pep3": {0: nan, 1: nan, 2: 23.6, 3: nan, 4: nan}, # 4 values replace based on one (edge case)
+    "pep4": {0: nan, 1: nan, 2: nan, 3: nan, 4: nan},
+}
+
+df_test_data = pd.DataFrame(test_data)
+
+mask = df_test_data.isna()
+
+# floating point problem: numbers are not treated as decimals
+expected = {
+    (0, 'pep1'): (27.8 + 28.9) / 2,
+    (0, 'pep3'): 23.6,
+    (1, "pep2"): (29.1 + 27.6) / 2,
+    (1, "pep3"): 23.6,
+    (3, "pep1"): (28.9 + 28.7) / 2,
+    (3, "pep3"): 23.6,
+    (4, "pep2"): (27.6 + 29.1) / 2,
+    (4, "pep3"): 23.6
+}           
+
+actual = interpolate(df_test_data).to_dict()
+
+assert actual == expected
+assert df_test_data.equals(pd.DataFrame(test_data))
+
