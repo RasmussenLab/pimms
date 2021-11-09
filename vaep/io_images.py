32d81aef9503f19b6af57330160cc4ead9fc93b4
Henry <henry.webel@cpr.ku.dk>
2021-11-09 18:17:08 +0100

    :bug: make folder in case it does not exist
    - even in case parent folder do not even exist

--- vaep/io_images.py
+++ vaep/io_images.py
@@ -1,10 +1,15 @@
-import os
+import pathlib
 import logging
 logger = logging.getLogger(__name__)
 
-def _savefig(fig, name, folder='.', pdf=True):
+def _savefig(fig, name, folder:pathlib.Path ='.', pdf=True):
     """Save matplotlib Figure (having method `savefig`) as pdf and png."""
-    filename = os.path.join(folder, name)
-    fig.savefig(filename + '.png')
-    if pdf: fig.savefig(filename + '.pdf')
-    logger.info(f"Saved Figures to {filename}")
+    folder = pathlib.Path(folder)
+    folder.mkdir(exist_ok=True, parents=True)
+    fname = folder / name
+    fig.savefig(fname.with_suffix('.png'))
+    if pdf: fig.savefig( fname.with_suffix('.pdf'))
+    logger.info(f"Saved Figures to {fname}")
+
+
+
