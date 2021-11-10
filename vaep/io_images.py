5f48349aea23744736908660259edc3489918a57
Henry <henry.webel@cpr.ku.dk>
2021-11-10 11:57:07 +0100

    :bug: ensure that folders specified in name are created
    - folders specified in name tag are now also created
      (even several levels)

--- vaep/io_images.py
+++ vaep/io_images.py
@@ -5,8 +5,9 @@ logger = logging.getLogger(__name__)
 def _savefig(fig, name, folder:pathlib.Path ='.', pdf=True):
     """Save matplotlib Figure (having method `savefig`) as pdf and png."""
     folder = pathlib.Path(folder)
-    folder.mkdir(exist_ok=True, parents=True)
     fname = folder / name
+    folder = fname.parent # in case name specifies folders
+    folder.mkdir(exist_ok=True, parents=True)
     fig.savefig(fname.with_suffix('.png'))
     if pdf: fig.savefig( fname.with_suffix('.pdf'))
     logger.info(f"Saved Figures to {fname}")
