e26c0beb21bf21e1c4f42dcd025ebbdcaeb21299
Henry <henry.webel@cpr.ku.dk>
2021-11-11 17:13:43 +0100

    :bug: restrict imputation of consecutive missing values
    - don't use already imputed values for further imputation

--- vaep/tests/test_pandas.py
+++ vaep/tests/test_pandas.py
@@ -7,6 +7,7 @@ test_data = {
     "pep2": {0: 29.1, 1: nan, 2: 27.6, 3: 29.1, 4: nan},
     "pep3": {0: nan, 1: nan, 2: 23.6, 3: nan, 4: nan}, # 4 values replace based on one (edge case)
     "pep4": {0: nan, 1: nan, 2: nan, 3: nan, 4: nan},
+    "pep5": {0: 26.0, 1: 27.0, 2: nan, 3: nan, 4: nan},
 }
 
 df_test_data = pd.DataFrame(test_data)
@@ -22,7 +23,11 @@ expected = {
     (3, "pep1"): (28.9 + 28.7) / 2,
     (3, "pep3"): 23.6,
     (4, "pep2"): (27.6 + 29.1) / 2,
-    (4, "pep3"): 23.6
+    (4, "pep3"): 23.6,
+    (2, "pep5"): 27.0, 
+    # (3, "pep5"): nan, # dropped 
+    # (4, "pep5"): nan, # dropped
+    # all peptides from pep4 dropped as expected
 }           
 
 actual = interpolate(df_test_data).to_dict()
