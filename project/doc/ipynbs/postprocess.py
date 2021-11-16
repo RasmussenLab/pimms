e1016524ec8a20025dd45939f75584c81f75837e
enryh <henry.webel@cpr.ku.dk>
2021-11-16 14:58:16 +0100

    :sparkles: add link replacement to md processing (for gists)
    - images added to a comment in a gist,
      can be linked in markdown documents
      as these are hosted on github afterwards

--- project/doc/ipynbs/postprocess.py
+++ project/doc/ipynbs/postprocess.py
@@ -9,6 +9,8 @@ parser.add_argument('--file', '-i', type=str,
                     help='path to markdown input file.', required=True)
 parser.add_argument('--overwrite',
                     help='Overwrite input file?', required=False, default=False, action='store_true')
+parser.add_argument('--image-links',
+                    help='file with image links to use', type=str, required=False, default='')
 
 args = parser.parse_args()
 print("Arguments provided: ", args)
@@ -54,6 +56,23 @@ file_str, n = re.subn(regex, '</style>\n<table', file_str)
 print(f'Removed {n = } times.')
 
 
+
+if args.image_links:
+    urls_fpath = Path(args.image_links)
+    assert urls_fpath.exists(), f"File does not exist: {urls_fpath}"
+    with open(urls_fpath) as f:
+        png_links_to_use = f.readlines()
+        png_links_to_use = [s.strip() for s in png_links_to_use]
+    print("Got links:\n {}".format("\n - ".join(s for s in png_links_to_use)))
+    
+    regex = '\!\[png\]\(.*.png\)'
+
+    png_path_to_replace = re.findall(regex, file_str)
+    print("Found png images to replace:\n {}".format("\n - ".join(s for s in png_path_to_replace)))
+    assert len(png_links_to_use) == len(png_path_to_replace), "Number on links does not match number of png images."
+    
+    for png_link, url_link in zip(png_path_to_replace, png_links_to_use):
+        file_str = file_str.replace(png_link, url_link) # returns a copie
 
 if not args.overwrite:
     file = file.parent / f"{file.stem}_replaced{file.suffix}"
