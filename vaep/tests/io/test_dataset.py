451b8caad0dd77c0ec18df713665946090c5bc2e
Henry <henry.webel@cpr.ku.dk>
2021-12-06 18:43:55 +0100

    :art: update test functions and types

--- vaep/tests/io/test_dataset.py
+++ vaep/tests/io/test_dataset.py
@@ -7,9 +7,10 @@ import helpers
 from vaep.io.datasets import DatasetWithMaskAndNoTarget
 
 def test_DatasetWithMaskAndNoTarget():    
+
     with pytest.raises(ValueError):
         DatasetWithMaskAndNoTarget(df=np.random.rand(10, 5))
-    
+
     data = helpers.create_DataFrame()
     ds = DatasetWithMaskAndNoTarget(df=data)
     assert all(ds[-1][1] == torch.tensor([95, 96, 97, 98, 99], dtype=torch.int32))
