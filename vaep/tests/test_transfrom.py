352a0088b07d315936ecd434a6127e1113d68ca1
Henry <henry.webel@cpr.ku.dk>
2021-12-06 19:46:16 +0100

    :sparkles::bug: separate encode and decode pipeline
    - Imputer with Mask will change the behaviour (appends mask to columns)
    - Subselect some entries from the encode pipeline
      by the given name
    - ToDo: Possibly add an error message in case pipeline is different
    - Open Question: Allow entirely different decode pipeline?

--- vaep/tests/test_transfrom.py
+++ vaep/tests/test_transfrom.py
@@ -47,8 +47,9 @@ def test_Vaep_Pipeline():
     )
     from random_data import data
     df = pd.DataFrame(data)
+    mask = df.notna()
     # new procs, transform equal encode, inverse_transform equals decode
-    dae_transforms = VaepPipeline(df, pipeline=dae_default_pipeline)
+    dae_transforms = VaepPipeline(df, encode=dae_default_pipeline)
     res = dae_transforms.transform(df)
     assert type(res)  == pd.DataFrame
     with pytest.raises(ValueError):
@@ -63,3 +64,7 @@ def test_Vaep_Pipeline():
         _ = dae_transforms.inverse_transform(res.values)  # numpy.array
     with pytest.raises(ValueError):    
         _ = dae_transforms.inverse_transform(res.values[0])  # single sample
+    dae_transforms = VaepPipeline(df, encode=dae_default_pipeline, decode=['normalize'])
+    res = dae_transforms.transform(df)
+    res = dae_transforms.inverse_transform(res)
+    npt.assert_array_almost_equal(df.values[mask], res.values[mask])
