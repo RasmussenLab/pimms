b8c920fa73eec6fd5c3e79f5fb2d7183081b0e21
Henry <henry.webel@cpr.ku.dk>
2021-12-16 20:19:59 +0100

    :white_check_mark: Add simple passing test for Autoencoder
    - used to check if update works

---
+++ vaep/tests/test_ae.py
@@ -0,0 +1,25 @@
+import vaep
+from vaep.models import ae
+
+
+expected_repr = """Autoencoder(
+  (encoder): Sequential(
+    (0): Linear(in_features=100, out_features=30, bias=True)
+    (1): Tanh()
+    (2): Linear(in_features=30, out_features=10, bias=True)
+    (3): Tanh()
+  )
+  (decoder): Sequential(
+    (0): Linear(in_features=10, out_features=30, bias=True)
+    (1): Tanh()
+    (2): Linear(in_features=30, out_features=100, bias=True)
+  )
+)"""
+
+def test_basic_repr():
+    model = ae.Autoencoder(n_features=100, n_neurons=30)
+    actual_repr = repr(model)
+    assert actual_repr == expected_repr
+    assert model.dim_latent == 10
+    assert model.n_neurons == 30
+    assert model.n_features == 100
