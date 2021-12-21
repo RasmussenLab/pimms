7173c23e56bffa22b103090c466e7c8c5f20ff0d
enryh <henry.webel@cpr.ku.dk>
2021-12-21 17:28:09 +0100

    :sparkles: get_latent_space helper function from model
    - use for plotting in analysis scripts

--- vaep/model.py
+++ vaep/model.py
@@ -139,6 +139,48 @@ def build_df_from_pred_batches(pred, scaler=None, index=None, columns=None):
     return pred
 
 
+def get_latent_space(model_method_call:callable,
+                     dl:torch.utils.data.DataLoader,
+                     dl_index:pd.Index,
+                     latent_tuple_pos:int=0) -> pd.DataFrame:
+    """Create a DataFrame of the latent space based on the model method call
+    to be used (here: the model encoder or a latent space helper method)
+
+    Parameters
+    ----------
+    model_method_call : callable
+        A method call on a pytorch.Module to create encodings for a batch of data.
+    dl : torch.utils.data.DataLoader
+        The DataLoader to use, producing predictions in a non-random fashion.
+    dl_index : pd.Index
+        pandas Index
+    latent_tuple_pos : int, optional
+        if model_method_call returns a tuple from a batch,
+        the tensor at the tuple position to selecet, by default 0
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame of latent space indexed with dl_index.
+    """
+    latent_space = []
+    for b in dl:
+        model_input = b[1]
+        res = model_method_call(model_input)
+        #if issubclass(type(res), torch.Tensor):
+        if isinstance(res, tuple):
+            res = res[latent_tuple_pos]
+        res = res.detach().numpy()
+        latent_space.append(res)
+    M = res.shape[-1]
+
+    latent_space = build_df_from_pred_batches(latent_space,
+                                              index=dl_index,
+                                              columns=[f'latent dimension {i+1}'
+                                                       for i in range(M)])
+    return latent_space
+
+
 def process_train_loss(d: dict, alpha=0.1):
     """Process training loss to DataFrame.
 
