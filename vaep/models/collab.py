debcf5a090518ce494f701b219634be024d426e9
Henry <henry.webel@cpr.ku.dk>
2021-12-30 11:44:07 +0100

    :art: helper function for collab dataloader creation
    - passing training and validation data is tighly coupled
      (might need adaptions in future)
    - Based on a long-data format with a set index
    - training and validation data in to separate DataFrames

--- vaep/models/collab.py
+++ vaep/models/collab.py
@@ -1,11 +1,17 @@
 import logging
+from typing import Tuple, List
 
-logger = logging.getLogger(__name__)
+import pandas as pd
 
 from fastai.collab import Module, Embedding, sigmoid_range
 from fastai.collab import EmbeddingDotBias
 
+logger = logging.getLogger(__name__)
+
+
 class DotProductBias(Module):
+    """Explicit implementation of fastai.collab.EmbeddingDotBias."""
+
     def __init__(self, n_samples, n_peptides, dim_latent_factors, y_range=(14, 30)):
         self.sample_factors = Embedding(n_samples, dim_latent_factors)
         self.sample_bias = Embedding(n_samples, 1)
@@ -22,3 +28,29 @@ class DotProductBias(Module):
         return sigmoid_range(res, *self.y_range)
 
 
+def combine_data(train_df: pd.DataFrame, val_df: pd.DataFrame) -> Tuple[pd.DataFrame, List[List[int]]]:
+    """Helper function to combine training and validation data in long-format. Returns
+    additionally list of list of row indices for each split for further use in fastai.
+
+    Parameters
+    ----------
+    train_df : pd.DataFrame
+        Consecutive training data in long-format, each row having (unit, feature, value)
+    val_df : pd.DataFrame
+        Consecutive training data in long-format, each row having (unit, feature, value)
+
+    Returns
+    -------
+    Tuple[pd.DataFrame, List[list, list]]
+        Pandas DataFrame of concatenated samples of training and validation data.
+        List of list of indices belonging to training data and list of indices belonging
+        to validation data.
+    """
+    X = train_df.append(val_df).reset_index()
+
+    # idx_splitter = IndexSplitter(list(range(len(data.train_X), len(data.train_X)+ len(data.val_X) )))
+    # splits = idx_splitter(ana_collab.X)
+    N_train, N_valid = len(train_df), len(val_df)
+    splits = [list(range(0, N_train)), list(range(N_train, N_train + N_valid))]
+
+    return X, splits
