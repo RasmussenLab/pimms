e73445e182036e0eb1265761a11b5718c001f090
Henry <henry.webel@cpr.ku.dk>
2022-01-03 16:46:41 +0100

    :refactor::bug::sparkles: sample_data function
    - non passed argument fixed (frac)
    - test for consistency when setting a feature weight to zero
    - based on series
      (DataFrame with one column should be treated equally
      -> try to squeeze)

--- vaep/sampling.py
+++ vaep/sampling.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Tuple
 
 import pandas as pd
 
@@ -46,3 +46,44 @@ def frequency_by_index(df_long: pd.DataFrame, sample_index_to_drop: Union[str, i
     # same as in feature_frequency
     freq_per_feat = _df_feat.notna().groupby(level=0).sum()
     return freq_per_feat.squeeze()
+
+
+"""Doubly indexed series with sample index and feature index."""
+
+
+def sample_data(series: pd.Series, sample_index_to_drop: Union[str, int],
+                frac=0.95, weights: pd.Series = None,
+                random_state=42) -> Tuple[pd.Series, pd.Series]:
+    """[summary]
+
+    Parameters
+    ----------
+    series : pd.Series
+        Long-format data in pd.Series. Index name is feature name. 2 dimensional 
+        MultiIndex. 
+    sample_index_to_drop : Union[str, int]
+        Sample index (as str or integer Index position). Unit to group by (i.e. Samples)
+    frac : float, optional
+        Percentage of single unit (sample) to sample, by default 0.95
+    weights : pd.Series, optional
+        Weights to pass on for sampling on a single group, by default None
+    random_state : int, optional
+        Random state to use for sampling procedure, by default 42
+
+    Returns
+    -------
+    Tuple[pd.Series, pd.Series]
+        First series contains the entries sampled, whereas the second series contains the
+        entires not sampled from the orginally passed series.
+    """
+    index_names = series.index.names
+    new_column = index_names[sample_index_to_drop]
+    df = series.to_frame('intensity').reset_index(sample_index_to_drop)
+
+    df_sampled = df.groupby(by=new_column).sample(
+        frac=frac, weights=weights, random_state=random_state)
+    series_sampled = df_sampled.reset_index().set_index(index_names).squeeze()
+
+    idx_diff = series.index.difference(series_sampled.index)
+    series_not_sampled = series.loc[idx_diff]
+    return series_sampled, series_not_sampled
