2c174366a05eca5fc281e87d1f3207d5f4d087fa
Henry <henry.webel@cpr.ku.dk>
2022-01-03 16:46:41 +0100

    :refactor::bug::sparkles: sample_data function
    - non passed argument fixed (frac)
    - test for consistency when setting a feature weight to zero
    - based on series
      (DataFrame with one column should be treated equally
      -> try to squeeze)

--- vaep/tests/test_sampling.py
+++ vaep/tests/test_sampling.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pytest
 
 from vaep.io.datasplits import to_long_format
-from vaep.sampling import feature_frequency, frequency_by_index
+from vaep.sampling import feature_frequency, frequency_by_index, sample_data
 
 from vaep.utils import create_random_df
 
@@ -12,7 +12,7 @@ from vaep.utils import create_random_df
 @pytest.fixture
 def random_data():
     """Fixture to load random data."""
-    return create_random_df(100, 15, prop_na=0.1)
+    return create_random_df(100, 10, prop_na=0.1)
 
 
 @pytest.fixture
@@ -38,3 +38,20 @@ def test_frequency_by_index(example_data):
                ==
                frequency_by_index(to_long_format(X),
                                   sample_index_to_drop=0))
+
+
+def test_sampling(random_data):
+    X = random_data
+    freq = feature_frequency(X)
+    excluded_feat = freq.sample(2).index.to_list()
+    freq.loc[excluded_feat] = 0
+    X = to_long_format(X).squeeze()
+    # ValueError: Fewer non-zero entries in p than size -> too many feat set to zero
+    series_sampled, series_not_sampled = sample_data(
+        X, 0, frac=0.70, weights=freq)
+    assert len(X) == len(
+        series_sampled) + len(series_not_sampled)
+    assert X.index.difference(
+        series_sampled.index.append(series_not_sampled.index)).empty
+    assert series_sampled.loc[pd.IndexSlice[:, excluded_feat]].empty
+    assert not series_not_sampled.loc[pd.IndexSlice[:, excluded_feat]].empty
