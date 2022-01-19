b8cfc323d88aaf5cc3fd0debb3e09ac2ed0397c5
Henry <henry.webel@cpr.ku.dk>
2022-01-19 12:19:55 +0100

    :sparkles::recycle::bug: RecorderDump for plotting saved losses
    - simple serialization using pickle of data related to plotting
      (fastai does not support this out of the box)
    - add suggested_initial_lr to model params dump

--- vaep/pandas.py
+++ vaep/pandas.py
@@ -1,3 +1,4 @@
+import collections.abc
 from collections import namedtuple
 from types import SimpleNamespace
 import pandas as pd
@@ -116,7 +117,6 @@ def _add_indices(array, original_df, index_only=False):
     return pd.DataFrame(array, index=index, columns=columns)
 
 
-
 def interpolate(wide_df: pd.DataFrame, name='replicates'):
     """Interpolate NA values with the values before and after.
     Uses n=3 replicates.
@@ -138,17 +138,43 @@ def interpolate(wide_df: pd.DataFrame, name='replicates'):
     mask = wide_df.isna()
     first_row = wide_df.iloc[0].copy()
     last_row = wide_df.iloc[-1].copy()
-    
+
     m = first_row.isna()
     first_row.loc[m] = wide_df.iloc[1:3, m.to_list()].mean()
-    
+
     m = last_row.isna()
     last_row.loc[m] = wide_df.iloc[-3:-1, m.to_list()].mean()
-    
-    ret = wide_df.interpolate(method='linear', limit_direction='both', limit=1, axis=0)
+
+    ret = wide_df.interpolate(
+        method='linear', limit_direction='both', limit=1, axis=0)
     ret.iloc[0] = first_row
     ret.iloc[-1] = last_row
-    
+
     ret = ret[mask].stack().dropna()
     ret.rename(name, inplace=True)
     return ret
+
+
+def flatten_dict_of_dicts(d: dict, parent_key: str = '') -> dict:
+    """Build tuples for nested dictionaries for use as `pandas.MultiIndex`.
+
+    Parameters
+    ----------
+    d : dict
+        Nested dictionary for which all keys are flattened to tuples.
+    parent_key : str, optional
+        Outer key (used for recursion), by default ''
+
+    Returns
+    -------
+    dict
+        Flattend dictionary with tuple keys: {(outer_key, ..., inner_key) : value}
+    """
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + (k,) if parent_key else (k,)
+        if isinstance(v, collections.abc.MutableMapping):
+            items.extend(flatten_dict_of_dicts(v, parent_key=new_key).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
