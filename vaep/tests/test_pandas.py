95d47758ac696fbea1950578c8e8d4bf49b7ced6
Henry <henry.webel@cpr.ku.dk>
2022-03-03 18:15:30 +0100

    :sparkles: rawfile metadata: group by MS instrument
    - group processed samples by MS instrument
    - allow training data selection based on MS instruments
      (by getting the indicies based on the selection)

--- vaep/tests/test_pandas.py
+++ vaep/tests/test_pandas.py
@@ -52,6 +52,29 @@ def test_flatten_dict_of_dicts():
 
     assert expected == actual
 
+def test_create_dict_of_dicts():
+    data = {('a', 'a1', 'a2'): 1,
+                ('a', 'a1', 'a3'): 2,
+                ('b', 'b1', 'b2'): 3,
+                ('b', 'b1', 'b3'): 4}
+    expected = {
+        "a": {'a1': {'a2': 1, 'a3': 2}},
+        "b": {'b1': {'b2': 3, 'b3': 4}}
+    }
+    actual = vaep.pandas.create_dict_of_dicts(data)
+    assert expected == actual
+
+    data = {('a', 'a1', 'a2'): (1, 1),
+                ('a', 'a1', 'a3'): (2, 2),
+                ('b', 'b1', 'b2'): (3, 3),
+                ('b', 'b1', 'b3'): (4, 4)}
+    expected = {
+        "a": {'a1': {'a2': [1, 1], 'a3': [2, 2]}},
+        "b": {'b1': {'b2': [3, 3], 'b3': [4, 4]}}
+    }
+    actual = vaep.pandas.create_dict_of_dicts(data, transform_values=list)
+    assert expected == actual
+
 
 def test_key_map():
     # Build a schema of dicts
