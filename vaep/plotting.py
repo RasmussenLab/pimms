5e1cca31a629b2b6c2e104e18d177f7fa6b2aca2
Henry <henry.webel@cpr.ku.dk>
2022-03-30 18:41:55 +0200

    :sparkles: plot intensity distribution
    - currently for all_peptides
    - in order to identify systematic shifts
      in sample medians and visualize boxplots

--- vaep/plotting.py
+++ vaep/plotting.py
@@ -2,6 +2,9 @@ import pathlib
 import logging
 
 import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -17,3 +20,58 @@ def _savefig(fig, name, folder:pathlib.Path ='.', pdf=True):
 
 
 savefig = _savefig
+
+
+def select_xticks(ax: matplotlib.axes.Axes, max_ticks: int = 50) -> list:
+    """Limit the number of xticks displayed.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        Axes object to manipulate
+    max_ticks : int, optional
+        maximum number of set ticks on x-axis, by default 50
+
+    Returns
+    -------
+    list
+        list of current ticks for x-axis. Either new
+        or old (depending if something was changed).
+    """
+    x_ticks = ax.get_xticks()
+    offset = len(x_ticks) // max_ticks
+    if offset > 1:  # if larger than 1
+        return ax.set_xticks(x_ticks[::offset])
+    return x_ticks
+
+def select_dates(date_series:pd.Series, max_ticks=30) -> np.array:
+    """Get unique dates (single days) for selection in pd.plot.line
+    with xticks argument.
+
+    Parameters
+    ----------
+    date_series : pd.Series
+        datetime series to use (values, not index)
+    max_ticks : int, optional
+        maximum number of unique ticks to select, by default 30
+
+    Returns
+    -------
+    np.array
+        _description_
+    """
+    xticks = date_series.dt.date.unique()
+    offset = len(xticks) // max_ticks
+    if offset > 1:
+        return xticks[::offset]
+    else:
+        xticks
+
+def make_large_descriptors():
+    """Helper function to have very large titles, labes and tick texts for 
+    matplotlib plots per default."""
+    plt.rcParams.update({'xtick.labelsize': 'xx-large',
+                         'ytick.labelsize': 'xx-large',
+                         'axes.titlesize':  'xx-large',
+                         'axes.labelsize':  'xx-large',
+                         })
