4d61873add670f9b247931b8cc5fa1e692b20ffa
Henry <henry.webel@cpr.ku.dk>
2022-05-03 18:14:12 +0200

    :recycle: ThermoRawFileParser keys
    - 14_experiment_03_data -> will be adapted to run with
       new defaults (defaults are out of sync)

---
+++ vaep/io/thermo_raw_files.py
@@ -0,0 +1,26 @@
+cols_instrument = ['Thermo Scientific instrument model',
+                   'instrument attribute',
+                   'instrument serial number', ]
+
+
+meta_raw_selected = [
+    'Content Creation Date',
+    'Thermo Scientific instrument model',
+    'instrument serial number',
+    'Software Version',
+    'Number of MS1 spectra',
+    'Number of MS2 spectra',
+    'Number of scans',
+    'MS max charge',
+    'MS max RT',
+    'MS min MZ',
+    'MS max MZ',
+    'MS scan range',
+    'mass resolution',
+    'Retention time range',
+    'Mz range',
+    'beam-type collision-induced dissociation',
+    'injection volume setting',
+    'dilution factor',
+]
+
