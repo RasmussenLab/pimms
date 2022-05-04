8397eecb6c83b51d7f886f0a9d087c3fb8002093
Henry <henry.webel@cpr.ku.dk>
2022-05-04 17:36:11 +0200

    :sparkles: Add support per sample
    - for entire dump of one type
    - for each dataset extracted (threshold could be applied)
    - create json and plots

---
+++ vaep/io/filenames.py
@@ -0,0 +1,14 @@
+import re
+import logging
+import functools
+
+logger = logging.getLogger(__name__)
+
+def read_number_from_str(fname:str, regex:str='M[0-9]*', strip:int=1) -> int:
+    M = re.search(regex, fname).group()
+    logger.info(f"Found: {M}")
+    M = int(M[strip:])
+    return M
+
+read_M_features = functools.partial(read_number_from_str, regex='M[0-9]*', strip=1)
+read_N_samples = functools.partial(read_number_from_str, regex='N[0-9]*', strip=1)
