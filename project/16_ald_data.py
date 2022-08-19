cda001e895092227926f60b3d1f98b7de4e40142
Henry <henry.webel@cpr.ku.dk>
2022-08-19 11:02:30 +0200

    :recycle: rename data all to df
    - each section can be run independently (after metadata block)
    - once data is selected, object can be overwritten
      (although this rather works from the cmd line as notebooks cache output references)

--- project/16_ald_data.py
+++ project/16_ald_data.py
@@ -186,18 +186,18 @@ raw_meta.to_csv(folder_data_out / 'raw_meta.csv')
 # ## (Aggregated) Peptide Data 
 
 # %%
-peptides = pd.read_table(fnames.plasma_aggPeptides, low_memory=False)
+df = pd.read_table(fnames.plasma_aggPeptides, low_memory=False)
 N_FRIST_META = 8
-peptides.shape
+df.shape
 
 # %%
-peptides
+df
 
 # %%
-peptides.iloc[:, :N_FRIST_META].describe(include='all')
+df.iloc[:, :N_FRIST_META].describe(include='all')
 
 # %%
-column_types = peptides.iloc[:, N_FRIST_META:].columns.to_series().apply(lambda s: tuple(s.split('.')[-2:]))
+column_types = df.iloc[:, N_FRIST_META:].columns.to_series().apply(lambda s: tuple(s.split('.')[-2:]))
 column_types.describe()  # .apply(lambda l: l[-1])
 
 # %%
@@ -205,29 +205,29 @@ column_types = ['.'.join(x for x in tup) for tup in list(column_types.unique())]
 column_types
 
 # %%
-peptides = peptides.set_index(list(peptides.columns[:N_FRIST_META])).sort_index(axis=1)
+df = df.set_index(list(df.columns[:N_FRIST_META])).sort_index(axis=1)
 
 # %%
-peptides.loc[:, peptides.columns.str.contains(VAR_PEP)]
+df.loc[:, df.columns.str.contains(VAR_PEP)]
 
 # %%
-peptides.iloc[:20, :6]
+df.iloc[:20, :6]
 
 # %% [markdown]
 # create new multiindex from column
 
 # %%
 sep = '.raw.'
-peptides.columns = pd.MultiIndex.from_tuples(peptides.columns.str.split().str[1].str.split(
+df.columns = pd.MultiIndex.from_tuples(df.columns.str.split().str[1].str.split(
     sep).to_series().apply(tuple), names=['Sample ID', 'vars'])
-peptides = peptides.stack(0)
-peptides
+df = df.stack(0)
+df
 
 # %% [markdown]
 # ### Index meta data
 
 # %% tags=[]
-meta = peptides.index.to_frame().reset_index(drop=True)
+meta = df.index.to_frame().reset_index(drop=True)
 meta
 
 # %%
@@ -261,15 +261,15 @@ meta.describe(include='all')
 
 # %%
 sel_cols = ['Sample ID', 'PEP.StrippedSequence', 'PEP.Quantity'] # selected quantity in last position
-sel_data = peptides.reset_index()[sel_cols].drop_duplicates().set_index(sel_cols[:2])
-sel_data
+df = df.reset_index()[sel_cols].drop_duplicates().set_index(sel_cols[:2])
+df
 
 # %%
-sel_data = sel_data.squeeze().dropna().astype(float).unstack()
-sel_data
+df = df.squeeze().dropna().astype(float).unstack()
+df
 
 # %%
-idx = sel_data.index.to_series()
+idx = df.index.to_series()
 idx = idx.str.extract(r'(Plate[\d]_[A-H]\d*)').squeeze()
 idx.name = 'Sample ID'
 idx.describe()
@@ -278,12 +278,12 @@ idx.describe()
 # - rawfile metadata -> keep 
 
 # %%
-sel_data = sel_data.set_index(idx)
-sel_data = sel_data.loc[idx_overlap_plasma]
-sel_data
+df = df.set_index(idx)
+df = df.loc[idx_overlap_plasma]
+df
 
 # %%
-des_data = sel_data.describe()
+des_data = df.describe()
 des_data
 
 # %% [markdown]
@@ -293,7 +293,7 @@ des_data
 # For one raw file no metadata could be extracted (`ERROR: Unable to access the RAW file using the native Thermo library.`)
 
 # %%
-idx_diff = sel_data.index.difference(raw_meta.index)
+idx_diff = df.index.difference(raw_meta.index)
 annotations.loc[idx_diff]
 
 # %%
@@ -302,7 +302,7 @@ kwargs = {'xlabel': 'peptide number ordered by completeness',
           'title': 'peptide measurement distribution'}
 
 ax = vaep.plotting.plot_counts(des_data.T.sort_values(by='count', ascending=False).reset_index(
-), feat_col_name='count', feature_name='Aggregated peptides', n_samples=len(sel_data), ax=None, **kwargs)
+), feat_col_name='count', feature_name='Aggregated peptides', n_samples=len(df), ax=None, **kwargs)
 
 fig = ax.get_figure()
 fig.tight_layout()
@@ -313,35 +313,35 @@ vaep.savefig(fig, name='data_aggPeptides_completness', folder=folder_run)
 
 # %%
 PROP_FEAT_OVER_SAMPLES = .25
-prop = des_data.loc['count'] / len(sel_data)
+prop = des_data.loc['count'] / len(df)
 selected = prop >= PROP_FEAT_OVER_SAMPLES
 selected.value_counts()
 
 # %%
-sel_data = sel_data.loc[:, selected]
-sel_data
+df = df.loc[:, selected]
+df
 
 # %% [markdown]
 # Dump selected data
 
 # %%
 fnames.sel_plasma_aggPeptids = folder_data_out / 'ald_plasma_aggPeptides.pkl'
-sel_data.to_pickle(fnames.sel_plasma_aggPeptids)
+df.to_pickle(fnames.sel_plasma_aggPeptids)
 
 # %% [markdown]
 # ## Protein Group data
 
 # %%
-pg = pd.read_csv(fnames.plasma_proteinGroups, low_memory=False)
+df = pd.read_csv(fnames.plasma_proteinGroups, low_memory=False)
 idx_cols = ['PG.ProteinAccessions', 'PG.Genes']
 N_FRIST_META = 3
-pg
+df
 
 # %%
-pg.iloc[:, :N_FRIST_META].describe(include='all')
+df.iloc[:, :N_FRIST_META].describe(include='all')
 
 # %%
-column_types = pg.iloc[:, N_FRIST_META:].columns.to_series().apply(lambda s: tuple(s.split('.')[-2:]))
+column_types = df.iloc[:, N_FRIST_META:].columns.to_series().apply(lambda s: tuple(s.split('.')[-2:]))
 column_types.describe()  # .apply(lambda l: l[-1])
 
 # %%
@@ -349,8 +349,8 @@ column_types = ['.'.join(x for x in tup) for tup in list(column_types.unique())]
 column_types # 'PG.Quantity' expected
 
 # %%
-pg = pg.set_index(list(pg.columns[:N_FRIST_META])).sort_index(axis=1)
-pg.loc[:, pg.columns.str.contains(VAR_PG)]
+df = df.set_index(list(df.columns[:N_FRIST_META])).sort_index(axis=1)
+df.loc[:, df.columns.str.contains(VAR_PG)]
 
 
 # %% [markdown]
@@ -358,13 +358,13 @@ pg.loc[:, pg.columns.str.contains(VAR_PG)]
 
 # %%
 def find_idx_to_drop(df:pd.DataFrame, idx_to_keep:list):
-    to_drop = [x for x in pg.index.names if not x in idx_to_keep]
+    to_drop = [x for x in df.index.names if not x in idx_to_keep]
     logger.info("Columnns to drop: {}".format(",".join((str(x) for x in to_drop))))
     return to_drop
     
-to_drop = find_idx_to_drop(pg, idx_cols)
-pg = pg.reset_index(level=to_drop, drop=True)
-pg.head()
+to_drop = find_idx_to_drop(df, idx_cols)
+df = df.reset_index(level=to_drop, drop=True)
+df.head()
 
 # %% [markdown]
 # extract long sample name (highly specific to task)
@@ -374,61 +374,61 @@ pg.head()
 # %%
 sep = '.raw.'
 # sep = '.htrms.'
-pg.columns = pd.MultiIndex.from_tuples(pg.columns.str.split().str[-1].str.split(
+df.columns = pd.MultiIndex.from_tuples(df.columns.str.split().str[-1].str.split(
     sep).to_series().apply(tuple), names=['Sample ID', 'vars'])
-pg = pg.stack(0)
-pg
+df = df.stack(0)
+df
 
 # %% [markdown]
 # ### Select Protein Group data
 
 # %%
-sel_data = pg[[VAR_PG]]
-sel_data
+df = df[[VAR_PG]]
+df
 
 # %%
-mask = sel_data['PG.Quantity'] == 'Filtered'
+mask = df['PG.Quantity'] == 'Filtered'
 print("No. of Filtered entries: ", mask.sum())
-sel_data = sel_data.loc[~mask]
-sel_data
+df = df.loc[~mask]
+df
 
 # %%
-sel_data.dtypes
+df.dtypes
 
 # %%
-sel_data = sel_data.squeeze().dropna().astype(float).unstack()
-sel_data
+df = df.squeeze().dropna().astype(float).unstack()
+df
 
 # %%
-gene_non_unique = sel_data.index.to_frame()["PG.Genes"].value_counts() > 1
+gene_non_unique = df.index.to_frame()["PG.Genes"].value_counts() > 1
 gene_non_unique = gene_non_unique[gene_non_unique].index
 gene_non_unique
 
 # %%
-sel_data.loc[pd.IndexSlice[:, gene_non_unique], :].T.describe()
+df.loc[pd.IndexSlice[:, gene_non_unique], :].T.describe()
 
 # %%
-sel_data = sel_data.T
+df = df.T
 
-idx = sel_data.index.to_series()
+idx = df.index.to_series()
 idx = idx.str.extract(r'(Plate[\d]_[A-H]\d*)').squeeze()
 idx.name = 'Sample ID'
 idx.describe()
 
 # %%
-sel_data = sel_data.set_index(idx)
-sel_data = sel_data.loc[idx_overlap_plasma]
-sel_data
+df = df.set_index(idx)
+df = df.loc[idx_overlap_plasma]
+df
 
 # %%
-des_data = sel_data.describe()
+des_data = df.describe()
 des_data
 
 # %% [markdown]
 # ### Check for metadata from rawfile overlap
 
 # %%
-idx_diff = sel_data.index.difference(raw_meta.index)
+idx_diff = df.index.difference(raw_meta.index)
 annotations.loc[idx_diff]
 
 # %%
@@ -437,7 +437,7 @@ kwargs = {'xlabel': 'protein group number ordered by completeness',
           'title': 'protein group measurement distribution'}
 
 ax = vaep.plotting.plot_counts(des_data.T.sort_values(by='count', ascending=False).reset_index(
-), feat_col_name='count', n_samples=len(sel_data), ax=None, **kwargs)
+), feat_col_name='count', n_samples=len(df), ax=None, **kwargs)
 
 fig = ax.get_figure()
 fig.tight_layout()
@@ -448,19 +448,19 @@ vaep.savefig(fig, name='data_proteinGroups_completness', folder=folder_run)
 
 # %%
 PROP_FEAT_OVER_SAMPLES = .25
-prop = des_data.loc['count'] / len(sel_data)
+prop = des_data.loc['count'] / len(df)
 selected = prop >= PROP_FEAT_OVER_SAMPLES
 selected.value_counts()
 
 # %%
-sel_data = sel_data.loc[:, selected]
-sel_data
+df = df.loc[:, selected]
+df
 
 # %% [markdown]
 # Check for non unique genes after dropping uncommon protein groups.
 
 # %%
-gene_non_unique = sel_data.columns.to_frame()["PG.Genes"].value_counts() > 1
+gene_non_unique = df.columns.to_frame()["PG.Genes"].value_counts() > 1
 gene_non_unique = gene_non_unique[gene_non_unique].index
 gene_non_unique
 
@@ -471,14 +471,14 @@ gene_non_unique
 # - alternative selection: per sample, select protein group with highest intensity per sample
 
 # %%
-sel_data.T.loc[pd.IndexSlice[:, gene_non_unique], :].T.describe()
+df.T.loc[pd.IndexSlice[:, gene_non_unique], :].T.describe()
 
 # %%
-sel_data = sel_data.droplevel(1, axis=1)
-sel_data
+df = df.droplevel(1, axis=1)
+df
 
 # %%
-sel_data.to_pickle(folder_data_out / 'ald_plasma_proteinGroups.pkl')
+df.to_pickle(folder_data_out / 'ald_plasma_proteinGroups.pkl')
 
 # %% [markdown]
 # # Liver samples
@@ -489,20 +489,19 @@ sel_data.to_pickle(folder_data_out / 'ald_plasma_proteinGroups.pkl')
 # %%
 idx_cols = ['PG.ProteinAccessions', 'PG.Genes', 'Sample ID']
 N_FRIST_META = 9
-# sel_data = sel_data.reset_index(level=to_remove, drop=True)
 
 # %%
-peptides = pd.read_table(fnames.liver_aggPeptides, low_memory=False)
-peptides.shape
+df = pd.read_table(fnames.liver_aggPeptides, low_memory=False)
+df.shape
 
 # %%
-peptides
+df
 
 # %%
-peptides.iloc[:, :N_FRIST_META].describe(include='all')
+df.iloc[:, :N_FRIST_META].describe(include='all')
 
 # %%
-column_types = peptides.iloc[:, N_FRIST_META:].columns.to_series().apply(lambda s: tuple(s.split('.')[-2:]))
+column_types = df.iloc[:, N_FRIST_META:].columns.to_series().apply(lambda s: tuple(s.split('.')[-2:]))
 column_types.describe()  # .apply(lambda l: l[-1])
 
 # %%
@@ -510,30 +509,30 @@ column_types = ['.'.join(x for x in tup) for tup in list(column_types.unique())]
 column_types
 
 # %%
-peptides = peptides.set_index(list(peptides.columns[:N_FRIST_META])).sort_index(axis=1)
+df = df.set_index(list(df.columns[:N_FRIST_META])).sort_index(axis=1)
 
 # %%
 VAR_PEP = 'EG.TotalQuantity'
-peptides.loc[:, peptides.columns.str.contains(VAR_PEP)]
+df.loc[:, df.columns.str.contains(VAR_PEP)]
 
 # %%
-peptides.columns[:10]
+df.columns[:10]
 
 # %% [markdown]
 # create new multiindex from column
 
 # %%
 sep = '.htrms.'
-peptides.columns = pd.MultiIndex.from_tuples(peptides.columns.str.split().str[1].str.split(
+df.columns = pd.MultiIndex.from_tuples(df.columns.str.split().str[1].str.split(
     sep).to_series().apply(tuple), names=['Sample ID', 'vars'])
-peptides = peptides.stack(0)
-peptides
+df = df.stack(0)
+df
 
 # %% [markdown]
 # ### Index meta data
 
 # %% tags=[]
-meta = peptides.index.to_frame().reset_index(drop=True)
+meta = df.index.to_frame().reset_index(drop=True)
 meta
 
 # %%
@@ -567,13 +566,13 @@ meta.describe(include='all')
 
 # %%
 sel_cols = ['Sample ID', 'PEP.StrippedSequence', VAR_PEP] # selected quantity in last position
-sel_data = peptides.reset_index()[sel_cols].drop_duplicates().set_index(sel_cols[:2]).squeeze()
-sel_data
+df = df.reset_index()[sel_cols].drop_duplicates().set_index(sel_cols[:2]).squeeze()
+df
 
 # %%
-mask = sel_data != 'Filtered'
-sel_data = sel_data.loc[mask].astype(float)
-sel_data.sort_index()
+mask = df != 'Filtered'
+df = df.loc[mask].astype(float)
+df.sort_index()
 
 # %% [markdown]
 # Select entry with maximum intensity of `duplicated entries`
@@ -581,21 +580,21 @@ sel_data.sort_index()
 # > change of variable and many duplicates -> could be PSM table? (close to evidence?)
 
 # %%
-mask_idx_duplicated = sel_data.index.duplicated(False)
-sel_data.loc[mask_idx_duplicated].sort_index()
+mask_idx_duplicated = df.index.duplicated(False)
+df.loc[mask_idx_duplicated].sort_index()
 
 # %%
-sel_data = vaep.pandas.select_max_by(df=sel_data.reset_index(), grouping_columns=sel_cols[:-1], selection_column=sel_cols[-1]).set_index(sel_cols[:-1])
+df = vaep.pandas.select_max_by(df=df.reset_index(), grouping_columns=sel_cols[:-1], selection_column=sel_cols[-1]).set_index(sel_cols[:-1])
 
 # %%
-assert sel_data.index.duplicated(False).sum() == 0 , "Still missing values"
+assert df.index.duplicated(False).sum() == 0 , "Still missing values"
 
 # %%
-sel_data = sel_data.unstack()
-sel_data
+df = df.unstack()
+df
 
 # %%
-idx = sel_data.index.to_series()
+idx = df.index.to_series()
 idx = idx.str.extract(r'(PlateS[\d]_[A-H]\d*)').squeeze()
 idx.name = 'Sample ID'
 idx.describe()
@@ -604,12 +603,12 @@ idx.describe()
 # - rawfile metadata -> keep 
 
 # %%
-sel_data = sel_data.set_index(idx)
-sel_data = sel_data.loc[sel_liver_samples]
-sel_data
+df = df.set_index(idx)
+df = df.loc[sel_liver_samples]
+df
 
 # %%
-des_data = sel_data.describe()
+des_data = df.describe()
 des_data
 
 # %% [markdown]
@@ -619,7 +618,7 @@ des_data
 # For one raw file no metadata could be extracted (`ERROR: Unable to access the RAW file using the native Thermo library.`)
 
 # %%
-# idx_diff = sel_data.index.difference(raw_meta.index)
+# idx_diff = df.index.difference(raw_meta.index)
 # annotations.loc[idx_diff]
 
 # %%
@@ -628,7 +627,7 @@ kwargs = {'xlabel': 'peptide number ordered by completeness',
           'title': 'peptide measurement distribution'}
 
 ax = vaep.plotting.plot_counts(des_data.T.sort_values(by='count', ascending=False).reset_index(
-), feat_col_name='count', feature_name='Aggregated peptides', n_samples=len(sel_data), ax=None, **kwargs)
+), feat_col_name='count', feature_name='Aggregated peptides', n_samples=len(df), ax=None, **kwargs)
 
 fig = ax.get_figure()
 fig.tight_layout()
@@ -639,20 +638,20 @@ vaep.savefig(fig, name='data_liver_aggPeptides_completness', folder=folder_run)
 
 # %%
 PROP_FEAT_OVER_SAMPLES = .25
-prop = des_data.loc['count'] / len(sel_data)
+prop = des_data.loc['count'] / len(df)
 selected = prop >= PROP_FEAT_OVER_SAMPLES
 selected.value_counts()
 
 # %%
-sel_data = sel_data.loc[:, selected]
-sel_data
+df = df.loc[:, selected]
+df
 
 # %% [markdown]
 # Dump selected data
 
 # %%
 fnames.sel_liver_aggPeptids = folder_data_out / 'ald_liver_aggPeptides.pkl'
-sel_data.to_pickle(fnames.sel_liver_aggPeptids)
+df.to_pickle(fnames.sel_liver_aggPeptids)
 
 # %% [markdown]
 # ## Protein Groups
