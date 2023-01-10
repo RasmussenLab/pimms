c79188f3222022e21e7e72a19ad9e27ad916a9d6
Henry <henry.webel@cpr.ku.dk>
2023-01-10 18:46:05 +0100

    :bug: regression boxplot return from pandas
    - Series with one entry, grab first axes
    - does it return an axis in other versions?
    
    - could be a pandas version issue -> to observe

--- project/01_0_split_data.py
+++ project/01_0_split_data.py
@@ -481,6 +481,7 @@ vaep.savefig(ax.get_figure(), fname)
 y_col = 'prop. missing'
 x_col = 'Intensity rounded (based on N observations)'
 ax = missing_by_median[[x_col, y_col]].plot.box(by=x_col)
+ax = ax[0] # returned series due to by argument?
 _ = ax.set_title('')
 _ = ax.set_ylabel(y_col)
 _ = ax.set_xlabel(x_col)
