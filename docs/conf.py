6ce33a1bb8704251aafdcd379fa420e12d1b83d8
Henry <henry.webel@cpr.ku.dk>
2023-02-13 10:36:26 +0100

    :memo::bug: update markdown parser for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,9 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark',
+extensions = ['myst_parser',
               'sphinx.ext.napoleon',
-              'sphinx_markdown_tables'
+            #   'sphinx_markdown_tables'
               ]
 
 # Add any paths that contain templates here, relative to this directory.
