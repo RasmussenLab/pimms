5a0c4ff5b2bb1c73f41aba7ce642541fb2900fe5
Henry <henry.webel@cpr.ku.dk>
2023-03-13 14:17:41 +0100

    :art: clean-up

--- project/erda_03_training_data.py
+++ project/erda_03_training_data.py
@@ -18,12 +18,12 @@
 # Use a set of (most) common peptides to create inital data sets
 #
 # - based on `Counter` over all outputs from search (here: MaxQuant)
+#    - keep based on threshold `FEAT_COMPLETNESS_CUTOFF` possible features
+#    - option: select samples based on `YEARS` (e.g. due constrain by a batch of strains)
+#    - collect in wide format data from output files
 
 # %%
-import yaml
-import json
-import random  # shuffle, seed
-import functools
+from functools import partial
 from pathlib import Path
 import logging
 import multiprocessing
@@ -34,9 +34,8 @@ import pandas as pd
 from tqdm.notebook import tqdm_notebook
 
 import vaep
-from vaep.io import data_objects
 
-import config as config
+import config
 
 # %%
 from typing import List
@@ -51,14 +50,19 @@ def select_files_by_parent_folder(fpaths:List, years:List):
 # %% [markdown]
 # ## Setup
 
-# %%
+# %% [tag=parameters]
 RANDOM_SEED: int = 42  # Random seed for reproducibility
 FEAT_COMPLETNESS_CUTOFF = 0.25 # Minimal proportion of samples which have to share a feature
 YEARS = ['2017','2018', '2019', '2020']
 SAMPLE_COL = 'Sample ID'
+OUT_FOLDER = 'data/selected/'
+FN_ID_OLD_NEW: str = 'data/rename/selected_old_new_id_mapping.csv' # selected samples with pride and original id
 
 
 # %%
+out_folder = Path(OUT_FOLDER) / cfg.NAME
+out_folder.mkdir(exist_ok=True, parents=True)
+
 
 # %% [markdown]
 # Select a specific config file
@@ -69,12 +73,9 @@ from config.training_data import peptides as cfg
 # from config.training_data import evidence as cfg
 # from config.training_data import proteinGroups as cfg
 
-{k: getattr(cfg, k) for k in dir(cfg) if not k.startswith('_')}
+cfg_dict = {k: getattr(cfg, k) for k in dir(cfg) if not k.startswith('_')}
+cfg_dict
 
-# %%
-out_folder = 'data/selected/'
-out_folder = Path(out_folder) / cfg.NAME
-out_folder.mkdir(exist_ok=True, parents=True)
 
 # %% [markdown]
 # Set defaults from file (allows to potentially overwrite parameters)
@@ -101,8 +102,7 @@ FNAME_COUNTER = cfg.FNAME_COUNTER
 # - path are to `.raw` raw files, not the output folder (could be changed)
 
 # %%
-fn_id_old_new: str = 'data/rename/selected_old_new_id_mapping.csv' # selected samples with pride and original id
-df_ids = pd.read_csv(fn_id_old_new)
+df_ids = pd.read_csv(FN_ID_OLD_NEW)
 df_ids
 
 # %% [markdown]
@@ -134,13 +134,24 @@ IDX_selected = counts.loc[mask].set_index('Sequence').index
 IDX_selected
 
 # %% [markdown]
-# ### Collect in parallel
+# ## Select Dumps
 
 # %%
 selected_dumps = df_ids["Sample ID"]
 selected_dumps = {k: counter.dumps[k] for k in selected_dumps}
 selected_dumps = list(selected_dumps.items())
 selected_dumps[:10]
+
+# %% [markdown]
+# potentially select meta data
+
+# %%
+# selected_dumps = select_files_by_parent_folder(list(counter.dumps.values()), years=YEARS)
+# print("Total number of files:", len(selected_dumps))
+# selected_dumps[-10:]
+
+# %% [markdown]
+# ## Collect present abesent pattern in parallel
 
 # %%
 N_WORKERS = 8
@@ -228,10 +239,7 @@ all.to_csv(fname.with_suffix('.csv'), chunksize=1_000)
 
 
 # %% [markdown]
-# ## Selected Features
-#
-# - index names should also match!
-# - if not-> rather use a list?
+# ## Collect intensities in parallel
 
 # %%
 def load_fct(path):
@@ -242,17 +250,16 @@ def load_fct(path):
     )
     return s
 
-all = None
-
-from functools import partial
+all = None # free memory
 
 collect_intensities = partial(collect, index=IDX, load_fct=load_fct)
 
 with multiprocessing.Pool(N_WORKERS) as p:
     all = list(
         tqdm_notebook(
-            p.imap(collect_intensities, np.array_split(selected_dumps, N_WORKERS)),
-            total=N_WORKERS,
+            p.imap(collect_intensities,
+                   np.array_split(selected_dumps, N_WORKERS)),
+                   total=N_WORKERS,
         )
     )  
     
@@ -267,137 +274,4 @@ fname = out_folder / config.insert_shape(all,  'intensities_wide_selected{}.pkl'
 all.to_pickle(fname)
 
 # %%
-# # %%time
-# all = all.T
-# all
-
-# %%
-all.to_pickle(fname)
-
-# %%
-selected_features = counts.loc[mask].set_index(counter.idx_names).sort_index().index
-# selected_features.name = 'Gene names' # needs to be fixed
-selected_features
-
-# %% [markdown]
-# ## Select Dumps
-
-# %%
-selected_dumps = select_files_by_parent_folder(list(counter.dumps.values()), years=YEARS)
-print("Total number of files:", len(selected_dumps))
-selected_dumps[-10:]
-
-# %% [markdown]
-# ## Load one dump
-#
-# - check that this looks like you expect it
-#
-
-# %%
-LOAD_DUMP(selected_dumps[0])
-
-# %% [markdown]
-# ## Process folders
-#
-# - potentially in parallel, aggregating results
-# - if needed: debug using two samples
-#
-# Design decisions
-# - long format of data with categorical features (to save memory)
-#
-
-# %%
-from typing import List, Callable
-from pandas.errors import EmptyDataError
-
-def process_folders(fpaths: List[Path],
-                    selected_features: pd.Index,
-                    load_folder: Callable,
-                    id_col='Sample ID',
-                    dtypes: dict = {
-                        'Sample ID': 'category',
-                        'Sequence': 'category'}) -> tuple:
-    print(f"started new process with {len(fpaths)} files.")
-    data_intensity = []
-    for i, fpath in enumerate(fpaths):
-        if not i % 10: print(f"File ({i}): {fpath}")
-        sample_name = fpath.stem
-        try:
-            # does some filtering
-            dump = load_folder(fpath)
-        except EmptyDataError:
-            logging.warning(f'Empty dump: {fpath}')
-            continue
-        except FileNotFoundError:
-            logging.warning(f'Missing dump: {fpath}')
-            continue
-        
-        # long data format
-        sequences_available = dump.index.intersection(selected_features)
-        dump = dump.loc[sequences_available, 'Intensity'].reset_index()
-        dump[id_col] = sample_name
-        dump = dump.astype(dtypes)
-        data_intensity.append(dump)
-    
-    data_intensity = pd.concat(data_intensity, copy=False, ignore_index=True)
-    data_intensity = data_intensity.astype(dtypes)
-    return data_intensity
-
-# # experiment
-# process_folders(selected_dumps[:2],
-#                 selected_features=selected_features,
-#                 load_folder=LOAD_DUMP,
-#                 dtypes=TYPES_DUMP)
-
-
-
-# %%
-# %%time
-process_folders_peptides = functools.partial(process_folders,
-                                             selected_features=selected_features,
-                                             load_folder=LOAD_DUMP,
-                                             dtypes=TYPES_DUMP)
-collected_dfs = data_objects.collect_in_chuncks(paths=selected_dumps,
-                                                process_chunk_fct=process_folders_peptides,
-                                                chunks=200,
-                                                n_workers=1 # to debug, don't multiprocess
-                                               )
-
-# one would need to aggregate categories first to keep them during aggregation?
-collected_dfs = pd.concat(collected_dfs, copy=False, ignore_index=True)
-collected_dfs = collected_dfs.astype(TYPES_DUMP)
-df_intensities = collected_dfs
-df_intensities
-
-# %% [markdown]
-# Except Intensities everything should be of data type category in order to save memory
-
-# %%
-df_intensities.dtypes
-
-# %%
-df_intensities.describe(include='all')
-
-# %% [markdown]
-# Check how many samples could be loaded, set total number of features
-
-# %%
-N = df_intensities[SAMPLE_COL].nunique()
-M = len(selected_features)
-N,M
-
-# %% [markdown]
-# set index columns provided and squeeze to series
-
-# %%
-df_intensities = df_intensities.set_index(IDX_COLS_LONG).squeeze()
-
-# %%
-base_name = f'{BASE_NAME}_' + '_'.join(YEARS)
-fname = config.FOLDER_DATA / config.insert_shape(df_intensities,  base_name + '{}.pkl', shape=(N,M))
-print(f"{fname = }")
-df_intensities.to_pickle(fname)
-
-# %% [markdown]
-# - Only binary pickle format works for now
-# - csv and reshaping the data needs to much memory for a single erda instance with many samples
+all.to_csv(fname.with_suffix('.csv'), chunksize=1_000)
