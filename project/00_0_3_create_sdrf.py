6c7ca814f1f0c6be75c544167c33a0007e1a1fd9
Henry <henry.webel@cpr.ku.dk>
2023-05-10 18:38:35 +0200

    :sparkles::bug: Create dummy files locally, fix SDRF
    - check all files are in list of files (queried from server)
    - create some dummy files (placeholders) locally for pride submission tool
    - manuelly annotate the submission.px text file from the
      submission tool (basically: add files)
    - :bug: SDRF file had ontology issues
      (and cellline template was not enough)

--- project/00_0_3_create_sdrf.py
+++ project/00_0_3_create_sdrf.py
@@ -17,11 +17,16 @@ df_meta = pd.read_csv(fn_meta, index_col='new_sample_id')
 df_meta
 
 # %%
-sdrf = pd.read_table(fn_sdrf_cellline_template)
+sdrf = pd.DataFrame() # pd.read_table(fn_sdrf_cellline_template)
 sdrf['source name'] = df_meta.index
 sdrf = sdrf.set_index('source name')
 sdrf['characteristics[organism]'] = 'Homo sapiens'
 sdrf['characteristics[organism part]'] = 'cervex'
+sdrf['characteristics[ancestry category]'] = 'Black'
+sdrf['characteristics[age]'] = '31Y'
+sdrf['characteristics[developmental stage]'] = 'adult'
+sdrf['characteristics[sex]'] = 'female'
+sdrf['characteristics[cell line]'] = 'HeLa cells'
 sdrf['characteristics[cell type]'] = 'epithelial'
 sdrf['characteristics[disease]'] = 'adenocarcinoma'
 sdrf['characteristics[cell line]'] = 'HeLa cells'
@@ -45,71 +50,73 @@ sdrf
 # Exactive Plus MS:1002526
 # Q Exactive MS:1001911
 # Orbitrap Fusion Lumos MS:1002732
-# Q Exactive MS:1001911
+
 
 instrument_ms_mapping = {
-    'Q-Exactive-HF-X-Orbitrap_6070': 'NT=Q-Exactive-HF-X-Orbitrap_6070;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6071': 'NT=Q-Exactive-HF-X-Orbitrap_6071;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6075': 'NT=Q-Exactive-HF-X-Orbitrap_6075;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6101': 'NT=Q-Exactive-HF-X-Orbitrap_6101;AC=MS:1002877:',
-    'Q-Exactive-HF-Orbitrap_207': 'NT=Q-Exactive-HF-Orbitrap_207;AC=MS:1002523',
-    'Q-Exactive-HF-X-Orbitrap_6096': 'NT=Q-Exactive-HF-X-Orbitrap_6096;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6078': 'NT=Q-Exactive-HF-X-Orbitrap_6078;AC=MS:1002877:',
-    'Q-Exactive-HF-Orbitrap_147': 'NT=Q-Exactive-HF-Orbitrap_147;AC=MS:1002523',
-    'Q-Exactive-Orbitrap_1': 'NT=Q-Exactive-Orbitrap_1;AC=MS:1001911',
-    'Q-Exactive-HF-Orbitrap_143': 'NT=Q-Exactive-HF-Orbitrap_143;AC=MS:1002523',
-    'Q-Exactive-HF-Orbitrap_204': 'NT=Q-Exactive-HF-Orbitrap_204;AC=MS:1002523',
-    'Q-Exactive-HF-X-Orbitrap_6011': 'NT=Q-Exactive-HF-X-Orbitrap_6011;AC=MS:1002877:',
-    'Q-Exactive-HF-Orbitrap_206': 'NT=Q-Exactive-HF-Orbitrap_206;AC=MS:1002523',
-    'Q-Exactive-HF-X-Orbitrap_6073': 'NT=Q-Exactive-HF-X-Orbitrap_6073;AC=MS:1002877:',
-    'Q-Exactive-HF-Orbitrap_1': 'NT=Q-Exactive-HF-Orbitrap_1;AC=MS:1002523',
-    'Q-Exactive-HF-Orbitrap_148': 'NT=Q-Exactive-HF-Orbitrap_148;AC=MS:1002523',
-    'Orbitrap-Fusion-Lumos_FSN20115': 'NT=Orbitrap-Fusion-Lumos_FSN20115;AC=MS:1002732',
-    'Q-Exactive-HF-X-Orbitrap_6016': 'NT=Q-Exactive-HF-X-Orbitrap_6016;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6004': 'NT=Q-Exactive-HF-X-Orbitrap_6004;AC=MS:1002877:',
-    'Orbitrap-Exploris-480_MA10132C': 'NT=Orbitrap-Exploris-480_MA10132C;AC=MS:1003028',
-    'Q-Exactive-HF-X-Orbitrap_6028': 'NT=Q-Exactive-HF-X-Orbitrap_6028;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6044': 'NT=Q-Exactive-HF-X-Orbitrap_6044;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6025': 'NT=Q-Exactive-HF-X-Orbitrap_6025;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6324': 'NT=Q-Exactive-HF-X-Orbitrap_6324;AC=MS:1002877:',
-    'Orbitrap-Exploris-480_MA10134C': 'NT=Orbitrap-Exploris-480_MA10134C;AC=MS:1003028',
-    'Q-Exactive-HF-X-Orbitrap_6022': 'NT=Q-Exactive-HF-X-Orbitrap_6022;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6043': 'NT=Q-Exactive-HF-X-Orbitrap_6043;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6013': 'NT=Q-Exactive-HF-X-Orbitrap_6013;AC=MS:1002877:',
-    'Q-Exactive-HF-X-Orbitrap_6023': 'NT=Q-Exactive-HF-X-Orbitrap_6023;AC=MS:1002877:',
-    'Exactive-Series-Orbitrap_6004': 'NT=Exactive-Series-Orbitrap_6004;AC=MS:1001911',
-    'Orbitrap-Exploris-480_Invalid_SN_0001': 'NT=Orbitrap-Exploris-480_Invalid_SN_0001;AC=MS:1003028',
-    'Orbitrap-Exploris-480_MA10215C': 'NT=Orbitrap-Exploris-480_MA10215C;AC=MS:1003028',
-    'Q-Exactive-HF-Orbitrap_2612': 'NT=Q-Exactive-HF-Orbitrap_2612;AC=MS:1002523',
-    'Q-Exactive-Plus-Orbitrap_1': 'NT=Q-Exactive-Plus-Orbitrap_1;AC=MS:1002526',
-    'Q-Exactive-Plus-Orbitrap_143': 'NT=Q-Exactive-Plus-Orbitrap_143;AC=MS:1002526',
-    'Orbitrap-Exploris-480_MA10130C': 'NT=Orbitrap-Exploris-480_MA10130C;AC=MS:1003028',
+    'Q-Exactive-HF-X-Orbitrap_6070': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6071': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6075': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6101': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-Orbitrap_207': 'NT=Q Exactive HF;AC=MS:1002523',
+    'Q-Exactive-HF-X-Orbitrap_6096': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6078': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-Orbitrap_147': 'NT=Q Exactive HF;AC=MS:1002523',
+    'Q-Exactive-Orbitrap_1': 'NT=Q Exactive;AC=MS:1001911',
+    'Q-Exactive-HF-Orbitrap_143': 'NT=Q Exactive HF;AC=MS:1002523',
+    'Q-Exactive-HF-Orbitrap_204': 'NT=Q Exactive HF;AC=MS:1002523',
+    'Q-Exactive-HF-X-Orbitrap_6011': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-Orbitrap_206': 'NT=Q Exactive HF;AC=MS:1002523',
+    'Q-Exactive-HF-X-Orbitrap_6073': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-Orbitrap_1': 'NT=Q Exactive HF;AC=MS:1002523',
+    'Q-Exactive-HF-Orbitrap_148': 'NT=Q Exactive HF;AC=MS:1002523',
+    'Orbitrap-Fusion-Lumos_FSN20115': 'NT=Orbitrap Fusion Lumos;AC=MS:1002732',
+    'Q-Exactive-HF-X-Orbitrap_6016': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6004': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Orbitrap-Exploris-480_MA10132C': 'NT=Orbitrap Exploris 480;AC=MS:1003028',
+    'Q-Exactive-HF-X-Orbitrap_6028': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6044': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6025': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6324': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Orbitrap-Exploris-480_MA10134C': 'NT=Orbitrap Exploris 480;AC=MS:1003028',
+    'Q-Exactive-HF-X-Orbitrap_6022': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6043': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6013': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Q-Exactive-HF-X-Orbitrap_6023': 'NT=Q Exactive HF-X;AC=MS:1002877:',
+    'Exactive-Series-Orbitrap_6004': 'NT=Q Exactive;AC=MS:1001911',
+    'Orbitrap-Exploris-480_Invalid_SN_0001': 'NT=Orbitrap Exploris 480;AC=MS:1003028',
+    'Orbitrap-Exploris-480_MA10215C': 'NT=Orbitrap Exploris 480;AC=MS:1003028',
+    'Q-Exactive-HF-Orbitrap_2612': 'NT=Q Exactive HF;AC=MS:1002523',
+    'Q-Exactive-Plus-Orbitrap_1': 'NT=Exactive Plus;AC=MS:1002526',
+    'Q-Exactive-Plus-Orbitrap_143': 'NT=Exactive Plus;AC=MS:1002526',
+    'Orbitrap-Exploris-480_MA10130C': 'NT=Orbitrap Exploris 480;AC=MS:1003028',
 }
 sdrf['comment[instrument]'] = sdrf['comment[instrument]'].replace(
     instrument_ms_mapping)
 
 # %%
-# change order: The column technology typecannot be before the assay name -- ERROR
+# change order: The column `technology type`` cannot be before the `assay name`` -- ERROR
 # template has wrong order (open PR)
-order = ['characteristics[organism]',
-         'characteristics[organism part]',
-         'characteristics[cell type]',
-         'characteristics[disease]',
-         'characteristics[cell line]',
-         'characteristics[biological replicate]',
-         'assay name',
-         'technology type',
-         'comment[technical replicate]',
-         'comment[data file]',
-         'comment[fraction identifier]',
-         'comment[label]',
-         'comment[cleavage agent details]',
-         'comment[instrument]']
+# -> done now above
+# order = ['characteristics[organism]',
+#          'characteristics[organism part]',
+#          'characteristics[ancestry category]',
+#          'characteristics[cell type]',
+#          'characteristics[disease]',
+#          'characteristics[cell line]',
+#          'characteristics[biological replicate]',
+#          'assay name',
+#          'technology type',
+#          'comment[technical replicate]',
+#          'comment[data file]',
+#          'comment[fraction identifier]',
+#          'comment[label]',
+#          'comment[cleavage agent details]',
+#          'comment[instrument]']
 
-sdrf = sdrf[order]
+# sdrf = sdrf[order]
 
 # %%
-fname = Path('data') / 'dev_datasets' / 'sdrf.tsv'
+fname = Path('data') / 'dev_datasets' / 'Experimental-Design.sdrf.tsv'
 sdrf.to_csv(fname, sep='\t')
 fname
 # %% [markdown]
