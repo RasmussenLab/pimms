c89c769246ba50967626d3da3d430c8ed4f775ff
Henry <henry.webel@cpr.ku.dk>
2023-05-15 19:15:46 +0200

    :art: update to pride file - mq_summaries.csv
    - relevant information of mq_summaries.csv also provided
      in metadata_pride.csv

--- project/00_1_hela_MQ_summaries.py
+++ project/00_1_hela_MQ_summaries.py
@@ -24,40 +24,38 @@
 import ipywidgets as widgets
 import yaml
 import numpy as np
-from vaep.io.data_objects import MqAllSummaries
+import pandas as pd
 import vaep
 
 from config import FN_ALL_SUMMARIES
 print(f"{FN_ALL_SUMMARIES = }")
 
 # %% tags=["parameters"]
-FN_ALL_SUMMARIES: str = 'data/processed/all_summaries.json' # MqAllSummaries json
+FN_ALL_SUMMARIES: str = 'data/mq_summaries.csv' # MqAllSummaries json
+
 
 # %%
-FN_ALL_SUMMARIES = FN_ALL_SUMMARIES
-
-# %%
-mq_all_summaries = MqAllSummaries(FN_ALL_SUMMARIES)
-mq_all_summaries.df.describe().T
+mq_all_summaries = pd.read_csv(FN_ALL_SUMMARIES, index_col=0)
+mq_all_summaries
 
 # %% [markdown]
 # Find unique columns, see [post](https://stackoverflow.com/a/54405767/9684872)
 
 # %%
 from vaep.pandas import unique_cols
-unique_cols(mq_all_summaries.df.Multiplicity), unique_cols(
-    mq_all_summaries.df["Variable modifications first search"])  # int, NA
+unique_cols(mq_all_summaries.Multiplicity), unique_cols(
+    mq_all_summaries["Variable modifications first search"])  # int, NA
 
 # %%
 from vaep.pandas import get_unique_non_unique_columns
-columns = get_unique_non_unique_columns(mq_all_summaries.df)
-mq_all_summaries.df[columns.unique]
+columns = get_unique_non_unique_columns(mq_all_summaries)
+mq_all_summaries[columns.unique]
 
 # %%
-mq_all_summaries.df[columns.unique].dtypes
+mq_all_summaries[columns.unique].dtypes
 
 # %%
-mq_all_summaries.df[columns.unique].iloc[0, :]
+mq_all_summaries[columns.unique].iloc[0, :]
 
 
 # %% [markdown]
@@ -70,16 +68,16 @@ class col_summary:
     MS2_identified  = 'MS/MS Identified'
     peptides_identified = 'Peptide Sequences Identified'
 
-if mq_all_summaries.df is None:
+if mq_all_summaries is None:
     raise ValueError("No data assigned")
     
-MS_spectra = mq_all_summaries.df[[col_summary.MS1, col_summary.MS2, col_summary.MS2_identified, col_summary.peptides_identified]]
+MS_spectra = mq_all_summaries[[col_summary.MS1, col_summary.MS2, col_summary.MS2_identified, col_summary.peptides_identified]]
 
 def compute_summary(threshold_identified):
     mask  = MS_spectra[col_summary.peptides_identified] >= threshold_identified
     display(MS_spectra.loc[mask].describe(np.linspace(0.05, 0.95, 10)))
 
-w_ions_range = widgets.IntSlider(value=15_000, min=.0, max=MS_spectra[col_summary.peptides_identified].max())
+w_ions_range = widgets.IntSlider(value=15_000, min=15_000, max=MS_spectra[col_summary.peptides_identified].max())
 display(widgets.interactive(compute_summary, threshold_identified=w_ions_range))
 
 # %% [markdown]
@@ -102,4 +100,3 @@ dump_dict['files'] = MS_spectra.loc[mask].index.to_list()
 with open('data/samples_selected.yaml', 'w') as f:
     yaml.dump(dump_dict, stream=f)
 
-# %%
