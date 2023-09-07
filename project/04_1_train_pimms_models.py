860139e4f616d62824a1bd416f99fe58f3de56bc
Henry <henry.webel@cpr.ku.dk>
2023-09-07 16:49:21 +0200

    :art: data path and install separated, headings

--- project/04_1_train_pimms_models.py
+++ project/04_1_train_pimms_models.py
@@ -15,11 +15,16 @@ import os
 IN_COLAB = 'COLAB_GPU' in os.environ
 if IN_COLAB:
     print("Install PIMMS...")
-    # !pip install git+https://github.com/RasmussenLab/pimms.git@dev
-    # #  !pip install pimms-learn
+    # # !pip install git+https://github.com/RasmussenLab/pimms.git@dev
+    # # !pip install pimms-learn
+
+# %%
+import os
+IN_COLAB = 'COLAB_GPU' in os.environ
+
+fn_intensities = 'data/dev_datasets/HeLa_6070/protein_groups_wide_N50.csv'
+if IN_COLAB:
     fn_intensities = 'https://raw.githubusercontent.com/RasmussenLab/pimms/main/project/data/dev_datasets/HeLa_6070/protein_groups_wide_N50.csv'
-else:
-    fn_intensities = 'data/dev_datasets/HeLa_6070/protein_groups_wide_N50.csv'
 
 # %%
 import numpy as np
@@ -71,8 +76,11 @@ df.head()
 # %% [markdown]
 # The resulting DataFrame with one column has an `MulitIndex` with the sample and feature identifier.
 
+# %% [markdown]
+# ## Collaborative Filtering
+
 # %%
-# CollaborativeFilteringTransformer?
+# # # # CollaborativeFilteringTransformer?
 
 # %% [markdown]
 # Let's set up collaborative filtering without a validation or test set, using 
