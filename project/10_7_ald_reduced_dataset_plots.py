bbe068e4b72e5d99680e57868444711e9df5436d
Henry <henry.webel@cpr.ku.dk>
2023-12-05 09:45:58 +0100

    :art::wrench: Update comp. with subsetted data
    - use a share of 25% MNAR in removed data
    - use a share of 25% MNAR in comparison
    - update figures for publication (names, label, fontsize, etc)

--- project/10_7_ald_reduced_dataset_plots.py
+++ project/10_7_ald_reduced_dataset_plots.py
@@ -8,9 +8,19 @@ import pandas as pd
 
 import vaep
 plt.rcParams['figure.figsize'] = [4, 2]  # [16.0, 7.0] , [4, 3]
-vaep.plotting.make_large_descriptors(5)
+vaep.plotting.make_large_descriptors(6)
+
+
+NONE_COL_NAME = 'No imputation\n(None)'
+col_mapper = {'None':
+              NONE_COL_NAME}
+# overwrite for now to align with Fig. 3
+ORDER_MODELS = ['DAE', 'VAE', 'TRKNN', 'RF', 'CF', 'Median', 'QRILC', NONE_COL_NAME]
+REF_MODEL = 'None (100%)'
+CUTOFF = 0.05
 
 COLORS_TO_USE_MAPPTING = vaep.plotting.defaults.color_model_mapping
+COLORS_TO_USE_MAPPTING[NONE_COL_NAME] = COLORS_TO_USE_MAPPTING['None']
 
 
 def plot_qvalues(df, x: str, y: list, ax=None, cutoff=0.05,
@@ -34,7 +44,7 @@ def plot_qvalues(df, x: str, y: list, ax=None, cutoff=0.05,
 # %% [markdown]
 # DA analysis
 # %%
-out_folder = 'runs/appl_ald_data/plasma/proteinGroups_80%_dataset/diff_analysis/kleiner/'
+out_folder = 'runs/appl_ald_data_2023_11/plasma/proteinGroups_80perc_25MNAR/diff_analysis/kleiner/'
 out_folder = Path(out_folder)
 
 # %%
@@ -43,33 +53,27 @@ fname = out_folder / 'ald_reduced_dataset_plots.xlsx'
 files_out[fname.name] = fname.as_posix()
 writer = pd.ExcelWriter(fname)
 
-# %% [markdown]
-# Ordering of model and reference model
+
 
 # %%
-ORDER_MODELS = pd.read_csv(
-    out_folder.parent.parent / 'figures/performance_test.csv',
-    index_col=0
-).index.to_list()
-ORDER_MODELS
 
-# %%
-# overwrite for now to align with Fig. 3
-ORDER_MODELS = ['DAE', 'VAE', 'rf', 'CF', 'KNN', 'Median', 'None']
-REF_MODEL = 'None (100%)'
-CUTOFF = 0.05
 
 # %% [markdown]
 # Load dumps
 
 # %%
-da_target = pd.read_pickle(out_folder / 'equality_rejected_target.pkl')
+da_target = (pd
+             .read_pickle(out_folder / 'equality_rejected_target.pkl').
+             rename(col_mapper, axis=1)
+             )
 da_target.describe()
 
 # %%
-qvalues = pd.read_pickle(out_folder / 'qvalues_target.pkl')
+qvalues = (pd
+           .read_pickle(out_folder / 'qvalues_target.pkl')
+           .rename(col_mapper, axis=1)
+           )
 qvalues
-
 
 # %% [markdown]
 # take only those with different decisions
@@ -87,7 +91,7 @@ feat_idx_w_diff
 # %%
 qvalues_sel = (qvalues
                .loc[feat_idx_w_diff]
-               .sort_values(('None', 'qvalue')
+               .sort_values((NONE_COL_NAME, 'qvalue')
                             ))
 
 
@@ -100,7 +104,7 @@ da_target_sel
 
 # %%
 mask_lost_sign = (
-    (da_target_sel['None'] == False)
+    (da_target_sel[NONE_COL_NAME] == False)
     & (da_target_sel[REF_MODEL])
 )
 sel = qvalues_sel.loc[mask_lost_sign.squeeze()]
@@ -121,7 +125,7 @@ da_target_sel_counts = (da_target_sel[ORDER_MODELS]
 ).droplevel(-1, axis=1)
 )
 da_target_sel_counts = vaep.pandas.combine_value_counts(da_target_sel_counts)
-ax = da_target_sel_counts.T.plot.bar()
+ax = da_target_sel_counts.T.plot.bar(ylabel='count')
 ax.locator_params(axis='y', integer=True)
 fname = out_folder / 'lost_signal_da_counts.pdf'
 files_out[fname.name] = fname.as_posix()
@@ -132,7 +136,8 @@ ax = plot_qvalues(df=sel,
                   x=REF_MODEL,
                   y=ORDER_MODELS,
                   cutoff=CUTOFF)
-ax.set_xlim(-0.0005, CUTOFF + 0.0005)
+ax.set_xlim(-0.0005, CUTOFF + 0.015)
+ax.legend(loc='upper right')
 ax.set_xlabel("q-value using 100% of the data without imputation")
 ax.set_ylabel("q-value using 80% of the data")
 fname = out_folder / 'lost_signal_qvalues.pdf'
@@ -145,7 +150,7 @@ vaep.savefig(ax.figure, fname)
 
 # %%
 mask_gained_signal = (
-    (da_target_sel['None'])
+    (da_target_sel[NONE_COL_NAME])
     & (da_target_sel[REF_MODEL] == False)
 )
 sel = qvalues_sel.loc[mask_gained_signal.squeeze()]
@@ -164,7 +169,7 @@ da_target_sel_counts = (da_target_sel[ORDER_MODELS]
 ).droplevel(-1, axis=1)
 )
 da_target_sel_counts = vaep.pandas.combine_value_counts(da_target_sel_counts)
-ax = da_target_sel_counts.T.plot.bar()
+ax = da_target_sel_counts.T.plot.bar(ylabel='count')
 ax.locator_params(axis='y', integer=True)
 fname = out_folder / 'gained_signal_da_counts.pdf'
 files_out[fname.name] = fname.as_posix()
@@ -174,10 +179,10 @@ vaep.savefig(ax.figure, fname)
 ax = plot_qvalues(sel,
                   x=REF_MODEL,
                   y=ORDER_MODELS)
-ax.set_xlim(CUTOFF - 0.01, sel[REF_MODEL].max() + 0.005)
+# ax.set_xlim(CUTOFF - 0.005, sel[REF_MODEL].max() + 0.005)
 ax.set_xlabel("q-value using 100% of the data without imputation")
-ax.set_ylabel("q-value using 80%")
-ax.legend(loc='upper center')
+ax.set_ylabel("q-value using 80% of the data")
+ax.legend(loc='upper right')
 fname = out_folder / 'gained_signal_qvalues.pdf'
 files_out[fname.name] = fname.as_posix()
 vaep.savefig(ax.figure, fname)
