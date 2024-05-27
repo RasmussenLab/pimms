8fb9fcc493eb0734dcf66f75697d427cbfa61db7
enryH <enryH@users.noreply.github.com>
2024-05-27 15:56:26 +0000

    deploy: 4774835b423750ae49b0ded838b85b19b54d861d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+ga2f76fe',
+    VERSION: '0.1.dev1+g4774835',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
