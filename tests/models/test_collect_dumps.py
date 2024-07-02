9d6858b7df537d53cfc7aa304abec928da78731f
Henry <henry.webel@cpr.ku.dk>
2024-07-02 09:58:00 +0200

    :bug: fix unit-test
    - wasnt' run before apparently

--- tests/models/test_collect_dumps.py
+++ tests/models/test_collect_dumps.py
@@ -5,5 +5,8 @@ def test_select_content():
     test_cases = ['model_metrics_HL_1024_512_256_dae',
                   'model_metrics_HL_1024_512_vae',
                   'model_metrics_collab']
-    for test_case in test_cases:
-        assert select_content(test_case, first_split='metrics_') == test_case.split('metrics_')[1]
+    expected = ['HL_1024_512_256',
+                'HL_1024_512',
+                'collab']
+    for test_case, v in zip(test_cases, expected):
+        assert select_content(test_case, first_split='metrics_') == v
